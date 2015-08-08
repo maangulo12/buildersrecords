a86222fd3be54e41e5ddc411eb8004e49e3a1f2d
Miguel Angulo <maangulo12@gmail.com>
2015-08-08 15:49:32 -0500

    Update v0.8

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,18 +1,14 @@
-var app = angular.module('app', ['ngRoute']);
+var app = angular.module('app', ['ngRoute', 'controllers']);
 
 app.config(function ($routeProvider) {
-	$routeProvider
-		.when('/', {
-			templateUrl: '../partials/view1.html',
-			controller: 'AppController'
-		})
-		.when('/view2', {
-			templateUrl: '../partials/view2.html',
-			controller: 'AppController'
-		})
-		.otherwise({ redirectTo: '/' });
-});
-
-app.controller('AppController', function ($scope) {
-    $scope.message = "I'm running Flask and AngularJS!";
+    $routeProvider
+	    .when('/', {
+            templateUrl: '../partials/home.html',
+		    controller: 'HomeController'
+	    })
+        .when('/signup', {
+		    templateUrl: '../partials/signup.html',
+		    controller: 'SignupController'
+	    })
+        .otherwise({ redirectTo: '/' });
 });
---
+++ app/static/js/controllers.js
@@ -0,0 +1,9 @@
+var controllers = angular.module('controllers', []);
+
+controllers.controller('HomeController', function ($scope) {
+    $scope.message = "I'm running Flask and AngularJS!";
+});
+
+controllers.controller('SignupController', function ($scope) {
+    
+});
