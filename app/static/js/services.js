c850e2576394bff32e4525e31e495866d733c5e2
Miguel Angulo <maangulo12@gmail.com>
2015-08-11 13:28:43 -0500

    First AJAX function
    Not working at the moment

--- app/static/js/services.js
+++ app/static/js/services.js
@@ -1,8 +1,21 @@
 var services = angular.module('services', []);
 
-services.service('UsersService', function() {
-    this.method1 = function() {
-        return 1;
+services.service('UsersService', function($http) {
+    // First AJAX function
+    this.getUsersList = function() {
+        $http.get('/api/users')
+            .then(function(response) {
+                console.log("MADE IT TO SUCCESS");
+                return response.data;
+                // this callback will be called asynchronously
+                // when the response is available
+                // NEED TO FIX THIS (not sending response.data back at the moment)
+            }, function(response) {
+                console.log("MADE IT TO ERROR");
+                return response.status;
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
     }
 
     this.method2 = function() {
