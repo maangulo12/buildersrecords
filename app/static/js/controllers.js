895d160622209c3628880df94e5c877c06e2c187
Miguel Angulo <maangulo12@gmail.com>
2015-08-11 07:06:17 -0500

    Add services into controller

--- app/static/js/controllers.js
+++ app/static/js/controllers.js
@@ -1,11 +1,11 @@
-var controllers = angular.module('controllers', []);
+var controllers = angular.module('controllers', ['services']);
 
 controllers.controller('HomeController', function ($scope) {
     $scope.message = "I'm running Flask and AngularJS!";
 });
 
-controllers.controller('SignupController', function ($scope) {
-
+controllers.controller('SignupController', function ($scope, UsersService) {
+    $scope.value = UsersService.method2();
 });
 
 controllers.controller('EimadFormController', function ($scope) {
