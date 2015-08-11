0cea809deea0f360514b3650cb729abe436eb227
Miguel Angulo <maangulo12@gmail.com>
2015-08-11 13:28:14 -0500

    Remove spaces

--- app/static/js/controllers.js
+++ app/static/js/controllers.js
@@ -1,13 +1,13 @@
 var controllers = angular.module('controllers', ['services']);
 
-controllers.controller('HomeController', function ($scope) {
+controllers.controller('HomeController', function($scope) {
     $scope.message = "I'm running Flask and AngularJS!";
 });
 
-controllers.controller('SignupController', function ($scope, UsersService) {
-    $scope.value = UsersService.method2();
+controllers.controller('SignupController', function($scope, UsersService) {
+    $scope.users = UsersService.getUsersList();
 });
 
-controllers.controller('EimadFormController', function ($scope) {
+controllers.controller('EimadFormController', function($scope) {
 
 });
