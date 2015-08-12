4b4e3735d0ab6fe99867765ebaad81c005822a24
Miguel Angulo <maangulo12@gmail.com>
2015-08-11 23:57:57 -0500

    Update SignupController
    Now using http request for AJAX

--- app/static/js/controllers.js
+++ app/static/js/controllers.js
@@ -1,11 +1,35 @@
-var controllers = angular.module('controllers', ['services']);
+var controllers = angular.module('controllers', []);
 
 controllers.controller('HomeController', function($scope) {
     $scope.message = "I'm running Flask and AngularJS!";
 });
 
-controllers.controller('SignupController', function($scope, UsersService) {
-    $scope.users = UsersService.getUsersList();
+controllers.controller('SignupController', function($scope, $http) {
+    $http.get('/api/users')
+        .then(function(response) {
+            $scope.msg = response.data;
+
+        }, function(response) {
+            $scope.msg = response.status;
+        }
+    );
+
+    // Post request: It Works!!!
+    $scope.addUser = function() {
+        $http.post('/api/users', {
+            username: 'mangulo',
+            pw_hash: 'password',
+            first_name: 'Miguel',
+            last_name: 'Angulo',
+            email: 'my_email@gmail.com'
+
+        }).then(function(response) {
+            $scope.status_sent = response.status;
+
+        }, function(response) {
+            $scope.status_sent = response.status;
+        });
+    }
 });
 
 controllers.controller('EimadFormController', function($scope) {
