e05ec8d5126e2758e3f2373e7d1ea290a85c848c
Miguel Angulo <maangulo12@gmail.com>
2015-08-14 01:07:19 -0500

    Add first directive
    Need to fix browser error

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngRoute', 'controllers']);
+var app = angular.module('app', ['ngRoute', 'ngMessages', 'controllers']);
 
 app.config(function($routeProvider, $locationProvider) {
     $routeProvider
@@ -17,4 +17,28 @@ app.config(function($routeProvider, $locationProvider) {
         .otherwise({ redirectTo: '/' });
 
     $locationProvider.html5Mode(true);
+});
+
+// First directive
+app.directive('usernameAvailability', function($http) {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, ctrl) {
+            ctrl.$asyncValidators.usernameAvailability = function(username) {
+                // ngModel $asyncValidators error on browser
+                $http.get('/api/users?q={"filters":[{"name":"username","op":"equals","val":"' + username + '"}]}')
+                    .then(function(response) {
+                        if (response.data.num_results == 0) {
+                            ctrl.$setValidity('usernameAvailability', true);
+                        }
+                        else {
+                            ctrl.$setValidity('usernameAvailability', false);
+                        }
+                    }, function(response) {
+                        ctrl.$setValidity('usernameAvailability', false);
+                        console.log('API ERROR')
+                    });
+            };
+        }
+    }
 });
