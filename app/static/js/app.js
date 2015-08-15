dcab6e6ba028d1e8d4e8eb0003038866f8f0ceab
Miguel Angulo <maangulo12@gmail.com>
2015-08-14 22:58:20 -0500

    Add correct logic using $q

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -19,25 +19,31 @@ app.config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
 });
 
-// First directive
-app.directive('usernameAvailability', function($http) {
+app.directive('usernameAvailability', function($http, $q) {
     return {
         require: 'ngModel',
         link: function(scope, element, attrs, ctrl) {
             ctrl.$asyncValidators.usernameAvailability = function(username) {
-                // ngModel $asyncValidators error on browser
-                $http.get('/api/users?q={"filters":[{"name":"username","op":"equals","val":"' + username + '"}]}')
-                    .then(function(response) {
-                        if (response.data.num_results == 0) {
-                            ctrl.$setValidity('usernameAvailability', true);
-                        }
-                        else {
-                            ctrl.$setValidity('usernameAvailability', false);
-                        }
-                    }, function(response) {
+
+                var promise = $http.get('/api/users?q={"filters":[{"name":"username","op":"equals","val":"' + username + '"}]}');
+
+                // Success function
+                var success = function(response) {
+                    if (response.data.num_results == 0) {
+                        ctrl.$setValidity('usernameAvailability', true);
+                    }
+                    else {
                         ctrl.$setValidity('usernameAvailability', false);
-                        console.log('API ERROR')
-                    });
+                        return $q.reject(response);
+                    }
+                }
+                // Failure function
+                var failure = function(response) {
+                    ctrl.$setValidity('usernameAvailability', false);
+                    return $q.reject(response);
+                }
+
+                return promise.then(success, failure);
             };
         }
     }
