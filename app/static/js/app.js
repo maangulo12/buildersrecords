dd4e094637ce522ceb1ac479e80cda90e5a3a352
Miguel Angulo <maangulo12@gmail.com>
2015-08-16 16:20:14 -0500

    Move directives to separate module

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,6 +1,9 @@
-var app = angular.module('app', ['ngRoute', 'ngMessages', 'controllers']);
+angular.module('app', ['ngRoute',
+                       'ngMessages',
+                       'app.controllers',
+                       'app.directives'])
 
-app.config(function($routeProvider, $locationProvider) {
+.config(function($routeProvider, $locationProvider) {
     $routeProvider
         .when('/', {
             templateUrl: '../partials/home.html',
@@ -17,60 +20,4 @@ app.config(function($routeProvider, $locationProvider) {
         .otherwise({ redirectTo: '/' });
 
     $locationProvider.html5Mode(true);
-});
-
-app.directive('usernameAvailability', function($http, $q) {
-    return {
-        require: 'ngModel',
-        link: function(scope, element, attrs, ctrl) {
-            ctrl.$asyncValidators.usernameAvailability = function(username) {
-
-                var promise = $http.get('/api/users?q={"filters":[{"name":"username","op":"equals","val":"' + username + '"}]}');
-
-                var success = function(response) {
-                    if (response.data.num_results == 0) {
-                        ctrl.$setValidity('usernameAvailability', true);
-                    }
-                    else {
-                        ctrl.$setValidity('usernameAvailability', false);
-                        return $q.reject(response);
-                    }
-                }
-                var failure = function(response) {
-                    ctrl.$setValidity('usernameAvailability', false);
-                    return $q.reject(response);
-                }
-
-                return promise.then(success, failure);
-            };
-        }
-    }
-});
-
-app.directive('emailAvailability', function($http, $q) {
-    return {
-        require: 'ngModel',
-        link: function(scope, element, attrs, ctrl) {
-            ctrl.$asyncValidators.emailAvailability = function(email) {
-
-                var promise = $http.get('/api/users?q={"filters":[{"name":"email","op":"equals","val":"' + email + '"}]}');
-
-                var success = function(response) {
-                    if (response.data.num_results == 0) {
-                        ctrl.$setValidity('emailAvailability', true);
-                    }
-                    else {
-                        ctrl.$setValidity('emailAvailability', false);
-                        return $q.reject(response);
-                    }
-                }
-                var failure = function(response) {
-                    ctrl.$setValidity('emailAvailability', false);
-                    return $q.reject(response);
-                }
-
-                return promise.then(success, failure);
-            };
-        }
-    }
 });
