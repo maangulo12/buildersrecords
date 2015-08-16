d779aa86ac7b138f2849df5e5fc262388bee778b
Miguel Angulo <maangulo12@gmail.com>
2015-08-16 16:21:11 -0500

    Create directives module

---
+++ app/static/js/directives.js
@@ -0,0 +1,53 @@
+angular.module('app.directives', ['app.services'])
+
+.directive('usernameAvailability', function(usersService, $q) {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, ctrl) {
+            ctrl.$asyncValidators.usernameAvailability = function(username) {
+
+                var promise = usersService.isUsernameUnique(username);
+                var success = function(response) {
+                    if (response.data.num_results == 0) {
+                        ctrl.$setValidity('usernameAvailability', true);
+                    }
+                    else {
+                        ctrl.$setValidity('usernameAvailability', false);
+                        return $q.reject(response);
+                    }
+                };
+                var failure = function(response) {
+                    ctrl.$setValidity('usernameAvailability', false);
+                    return $q.reject(response);
+                };
+                return promise.then(success, failure);
+            };
+        }
+    };
+})
+
+.directive('emailAvailability', function(usersService, $q) {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, ctrl) {
+            ctrl.$asyncValidators.emailAvailability = function(email) {
+
+                var promise = usersService.isEmailUnique(email);
+                var success = function(response) {
+                    if (response.data.num_results == 0) {
+                        ctrl.$setValidity('emailAvailability', true);
+                    }
+                    else {
+                        ctrl.$setValidity('emailAvailability', false);
+                        return $q.reject(response);
+                    }
+                };
+                var failure = function(response) {
+                    ctrl.$setValidity('emailAvailability', false);
+                    return $q.reject(response);
+                };
+                return promise.then(success, failure);
+            };
+        }
+    };
+});
