d6ce81138ecaddae945708cae30c71e6c708e7cc
Miguel Angulo <maangulo12@gmail.com>
2015-08-16 16:21:18 -0500

    Create services module

---
+++ app/static/js/services.js
@@ -0,0 +1,25 @@
+angular.module('app.services', [])
+
+.service('usersService', function($http) {
+    this.isUsernameUnique = function(username) {
+        return $http.get('/api/users?q={"filters":[{"name":"username","op":"equals","val":"' + username + '"}]}');
+    }
+
+    this.isEmailUnique = function(email) {
+        return $http.get('/api/users?q={"filters":[{"name":"email","op":"equals","val":"' + email + '"}]}');
+    }
+
+    this.getAllUsers = function() {
+        return $http.get('/api/users');
+    }
+
+    this.addUser = function(username, password, first_name, last_name, email) {
+        return $http.post('/api/users', {
+            username: username,
+            pw_hash: password,
+            first_name: first_name,
+            last_name: last_name,
+            email: email
+        });
+    }
+});
