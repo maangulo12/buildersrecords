8adc8abbe738443a341bd547be6e45b8be49dd9a
Miguel Angulo <maangulo12@gmail.com>
2015-08-16 19:05:36 -0500

    Move Flask-Bcrypt before Flask-SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,8 @@
 """
 
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bcrypt import Bcrypt
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.restless import APIManager
 
 
@@ -20,13 +20,13 @@ app = Flask(__name__, static_url_path = '')
 from app import settings
 from app import views
 
+# Flask-Bcrypt
+bcrypt = Bcrypt(app)
+
 # Flask-SQLAlchemy
 db = SQLAlchemy(app)
 from app import models
 db.create_all()
-
-# Flask-Bcrypt
-bcrypt = Bcrypt(app)
 
 # Flask-Restless
 api_manager = APIManager(app, flask_sqlalchemy_db = db)
