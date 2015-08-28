c47330fda1d4e2ff1dae30549e56bc8244cfa8e5
Miguel Angulo <maangulo12@gmail.com>
2015-08-27 20:40:39 -0500

    Remove comment

--- app/views.py
+++ app/views.py
@@ -11,8 +11,8 @@ from flask import make_response
 
 from app import app
 
-# NEED TO FIX THIS: Catch all url routes in Flask (not catching all routes at the moment)
-@app.route('/', defaults = { 'path': '' })
+
+@app.route('/', defaults = {'path': ''})
 @app.route('/<path>')
 def index(path):
     return make_response(open('app/templates/index.html').read())
