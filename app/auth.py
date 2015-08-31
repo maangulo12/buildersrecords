15c2e80be8492da276ebad0827bbcd0a392e5db1
Miguel Angulo <maangulo12@gmail.com>
2015-08-31 12:58:40 -0500

    Change method name

--- app/auth.py
+++ app/auth.py
@@ -16,7 +16,7 @@ from app.models import User
 @auth.verify_password
 def verify_password(username, password):
     user = User.query.filter_by(username = username).first()
-    if not user or not user.password_match(password):
+    if not user or not user.check_password(password):
         return False
     g.user = user
     return True
