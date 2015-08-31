c00045006de3c2f5b6529fd5d0733ca374e7f560
Miguel Angulo <maangulo12@gmail.com>
2015-08-31 14:04:07 -0500

    Remove Flask-RESTful

--- app/__init__.py
+++ app/__init__.py
@@ -12,26 +12,23 @@
 from flask import Flask
 from flask.ext.bcrypt import Bcrypt
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask_restful import Api
 from flask_httpauth import HTTPBasicAuth
 
 
 # Flask Application
 app = Flask(__name__, static_url_path = '')
 app.config.from_pyfile('settings.py')
-from app import views
 
 # Flask-Bcrypt
 bcrypt = Bcrypt(app)
 
 # Flask-SQLAlchemy
 db = SQLAlchemy(app)
-from app import models
-db.create_all()
-
-# Flask-RESTful
-api_manager = Api(app)
-from app import api
 
 # Flask-HTTPAuth
 auth = HTTPBasicAuth()
+
+# App Modules
+from app import models
+from app import api
+from app import views
