8270cfbffd28d3efa9a305b01e6e67d36afc2d0a
Miguel Angulo <maangulo12@gmail.com>
2015-08-31 12:59:18 -0500

    Update api

--- app/api.py
+++ app/api.py
@@ -3,17 +3,38 @@
 """
     app.api
     ~~~~~~~~~~~~~~~~
+
     API module
 """
 
-from flask_restful import Resource
+from flask_restful import Resource, fields, marshal_with
 
 from app import api_manager
-from app.models import User, Project, Category, Item
+from app.models import User
 
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+resource_fields = {
+    'username':   fields.String,
+    'password':   fields.String,
+    'first_name': fields.String,
+    'last_name':  fields.String,
+    'email':      fields.String
+}
 
-api_manager.add_resource(HelloWorld, '/api/helloworld')
+
+class Users(Resource):
+    @marshal_with(resource_fields)
+    def get(self, **kwargs):
+        list_obj = []
+        data = {
+            'num_results': len(list_obj),
+            'objects': list_obj
+        }
+        for user in User.query.all():
+            data = {
+                'num_results': len(list_obj),
+                'objects': list_obj.append(user.as_dict())
+            }
+        return data
+
+api_manager.add_resource(Users, '/api/users')
