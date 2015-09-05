db5f5c8744b33588855feb5f1c5d52484aa86891
Miguel Angulo <maangulo12@gmail.com>
2015-09-04 22:21:31 -0500

    Update settings

--- app/settings.py
+++ app/settings.py
@@ -10,28 +10,28 @@
 import os
 
 # Application Settings
-SECRET_KEY   =  os.environ.get('SECRET_KEY', 'secret_key')
+SECRET_KEY  = os.environ.get('SECRET_KEY', 'secret_key')
 
 # WSGI Server
-SERVER_HOST  =  os.environ.get('SERVER_HOST', '0.0.0.0')
-SERVER_PORT  =  int(os.environ.get('SERVER_PORT', 5555))
-DEBUG_FLAG   =  True
+SERVER_HOST = os.environ.get('SERVER_HOST', '0.0.0.0')
+SERVER_PORT = int(os.environ.get('SERVER_PORT', 5555))
+DEBUG_FLAG  = True
 
 # Local DB Settings
-DB_ENGINE    = 'postgresql'
-DB_USERNAME  = 'postgres'
-DB_PASSWORD  = 'password'
-DB_SERVER    = 'localhost'
-DB_PORT      = '5432'
-DB_NAME      = 'app_db'
+DB_ENGINE   = 'postgresql'
+DB_USERNAME = 'postgres'
+DB_PASSWORD = 'password'
+DB_SERVER   = 'localhost'
+DB_PORT     = '5432'
+DB_NAME     = 'app_db'
 
 # Production DB URL
-SQLALCHEMY_DATABASE_URI =  os.environ.get('DATABASE_URL', DB_ENGINE + '://' +
-                                                          DB_USERNAME + ':' +
-                                                          DB_PASSWORD + '@' +
-                                                          DB_SERVER + ':' +
-                                                          DB_PORT + '/' +
-                                                          DB_NAME)
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', DB_ENGINE + '://' +
+                                                         DB_USERNAME + ':' +
+                                                         DB_PASSWORD + '@' +
+                                                         DB_SERVER + ':' +
+                                                         DB_PORT + '/' +
+                                                         DB_NAME)
 # JWT
 JWT_USERNAME = os.environ.get('JWT_USERNAME', 'angular')
 JWT_PASSWORD = os.environ.get('JWT_PASSWORD', 'password')
