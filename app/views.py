4b8a00877c9c3a55c72f956be087d060caf7aaef
Miguel Angulo <maangulo12@gmail.com>
2015-09-04 21:02:56 -0500

    Update index route

--- app/views.py
+++ app/views.py
@@ -11,9 +11,9 @@ from flask import make_response
 
 from app import app
 
-
-@app.route('/')
-def index():
+@app.route('/', defaults = {'path': ''})
+@app.route('/<path>')
+def index(path):
     return make_response(open('app/templates/index.html').read())
 
 # NEED TO ADD ERROR VIEWS
