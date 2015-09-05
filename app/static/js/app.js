1033775da6bb3ad9e8dc6f141aa2c3423153bd3c
Miguel Angulo <maangulo12@gmail.com>
2015-09-04 19:32:21 -0500

    Update app

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,10 +1,16 @@
-angular.module('app', ['ngRoute',
-                       'ngMessages',
-                       'ui.validate',
-                       'app.controllers',
-                       'app.directives'])
+angular.module('app', [
+    'ngRoute',
+    'ngMessages',
+    'ui.validate',
+    'angular-jwt',
+    'angular-storage',
+    'app.controllers',
+    'app.directives',
+    'app.services'
+])
 
-.config(function($routeProvider, $locationProvider) {
+.config(function($routeProvider, $locationProvider, $httpProvider, jwtInterceptorProvider) {
+    // Routes
     $routeProvider
         .when('/', {
             templateUrl: '../partials/home.html',
@@ -14,11 +20,34 @@ angular.module('app', ['ngRoute',
             templateUrl: '../partials/signup.html',
             controller: 'SignupController'
         })
+        .when('/login', {
+            templateUrl: '../partials/login.html',
+            controller: 'LoginController'
+        })
         .when('/eimadForm', {
             templateUrl: '../partials/eimadForm.html',
             controller: 'EimadFormController'
         })
-        .otherwise({ redirectTo: '/' });
-
+        .otherwise({
+            redirectTo: '/'
+        });
     $locationProvider.html5Mode(true);
+
+    // Angular-jwt
+    jwtInterceptorProvider.tokenGetter = function(store) {
+        return store.get('jwt');
+    }
+    $httpProvider.interceptors.push('jwtInterceptor');
+})
+
+.run(function(authService, store) {
+    // authService - get Token
+    var promise = authService.authenticate();
+    var success = function(response) {
+        store.set('jwt', response.data.token);
+    };
+    var failure = function(response) {
+        store.remove('jwt');
+    };
+    promise.then(success, failure);
 });
