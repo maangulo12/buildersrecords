cb666b6ac56b56d81da461dc5fdcc015ed0c70ea
Miguel Angulo <maangulo12@gmail.com>
2015-09-07 23:05:09 -0500

    Update app

--- app/__init__.py
+++ app/__init__.py
@@ -20,25 +20,23 @@ from flask_mail import Mail
 # Flask Application
 app = Flask(__name__, static_url_path = '')
 app.config.from_pyfile('settings.py')
+from app import views
 
 # Flask-Bcrypt
 bcrypt = Bcrypt(app)
 
 # Flask-SQLAlchemy
 db = SQLAlchemy(app)
+from app import models
 
 # Flask-JWT
 jwt = JWT(app)
+from app import jwt
 
 # Flask-Restless
 api_manager = APIManager(app, flask_sqlalchemy_db = db)
+from app import api
 
 # Flask-Mail
 mail_service = Mail(app)
-
-# App Modules
-from app import models
-from app import jwt
 from app import mail
-from app import api
-from app import views
