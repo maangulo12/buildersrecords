ff1b748be525682632e1229541445eb919fcbf05
Miguel Angulo <maangulo12@gmail.com>
2015-09-19 21:16:09 -0500

    Update projects module

--- app/static/angular/projects/projects.js
+++ app/static/angular/projects/projects.js
@@ -14,20 +14,35 @@ angular.module('app.projects', [
 })
 .controller('ProjectsController', function($http, $scope, store, $state) {
     $scope.username = store.get('username');
+    getProjects();
 
+    // Get all of the user's projects
+    function getProjects() {
+        $http.get('/api/projects?q={"filters":[{"name":"user_id","op":"equals","val":"' + store.get('user_id') + '"}]}')
+        .then(function(response) {
+            $scope.projects = response.data.objects;
+        });
+    }
     $scope.logOut = function() {
         store.remove('jwt');
         $state.go('login');
     }
     $scope.clearFields = function() {
-        $scope.project_name = "";
+        $scope.project_name = '';
         $scope.new_project_form.$setPristine();
     }
     $scope.createProject = function() {
         $http.post('/api/projects', {
             project_name: $scope.project_name,
-            project_type: $scope.project_type,
             user_id: store.get('user_id')
+        })
+        .then(function(response) {
+            $("#close_button").click();
+            getProjects();
+            //$state.go('dashboard');
+        }, function(response) {
+            $scope.new_project_form.$invalid = true;
+            $scope.new_project_form.project_name.$invalid = true;
         });
     }
 });
