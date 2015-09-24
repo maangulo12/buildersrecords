9b933b28a2d70f6b2cba4e932f2a4e89ada06569
Miguel Angulo <maangulo12@gmail.com>
2015-09-24 01:37:21 -0500

    Fix getExpenditures function

--- app/static/angular/expenditures/expenditures.js
+++ app/static/angular/expenditures/expenditures.js
@@ -10,6 +10,7 @@ angular.module('app.expenditures', [
 })
 .controller('ExpendituresController', function($scope, store, $state) {
     $scope.username = store.get('username');
+    getExpenditures();
 
     $scope.logOut = function() {
         store.remove('jwt');
@@ -20,9 +21,9 @@ angular.module('app.expenditures', [
     }
     // GET EXPENDITURES function
     function getExpenditures() {
-        $http.get('/api/expenditures?q={"filters":[{"name":"user_id","op":"equals","val":"' + store.get('user_id') + '"}]}')
+        $http.get('/api/expenditures?q={"filters":[{"name":"project_id","op":"equals","val":"' + store.get('project_id') + '"}]}')
         .then(function(response) {
-            $scope.project_list = response.data.objects;
+            $scope.expenditure_list = response.data.objects;
         }, function(response) {
             // Could not load user's project
         });
