8ec62dee7f99b21edc4d9718d95fa391c5d26488
Miguel Angulo <maangulo12@gmail.com>
2015-09-24 01:33:58 -0500

    Update API
    Add expenditures api

--- app/api.py
+++ app/api.py
@@ -7,17 +7,18 @@
     This is the API module for the Flask-Restless extension.
 
     Current APIs:
-        -Users      : /api/users
-        -Projects   : /api/projects
-        -Categories : /api/categories
-        -Items      : /api/items
+        -Users        : /api/users
+        -Projects     : /api/projects
+        -Categories   : /api/categories
+        -Items        : /api/items
+        -Expenditures : /api/expenditures
 
     *Note: auth_func is imported in this module in order to protect API.
 """
 
 from app import api_manager
 from app.jwt import auth_func
-from app.models import User, Project, Category, Item
+from app.models import User, Project, Category, Item, Expenditure
 
 
 # Users: /api/users
@@ -85,3 +86,20 @@ api_manager.create_api(Item,
                             DELETE_SINGLE = [auth_func],
                             DELETE_MANY   = [auth_func]),
     collection_name  = 'items')
+
+# Expenditures: /api/expenditures
+# methods allowed: GET, POST, DELETE, PUT
+# results_per_page: pagination turned off (set to 0)
+# All methods are protected (all of them are listed in preprocessors)
+api_manager.create_api(Expenditure,
+    methods          = ['GET', 'POST', 'DELETE', 'PUT'],
+    url_prefix       = '/api',
+    results_per_page = 0,
+    preprocessors    = dict(POST          = [auth_func],
+                            GET_SINGLE    = [auth_func],
+                            GET_MANY      = [auth_func],
+                            PUT_SINGLE    = [auth_func],
+                            PUT_MANY      = [auth_func],
+                            DELETE_SINGLE = [auth_func],
+                            DELETE_MANY   = [auth_func]),
+    collection_name  = 'expenditures')
