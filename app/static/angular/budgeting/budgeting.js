9772a4f3249986eb7aaeb10f6ce709339b0a5573
amylopes <alopes5982@gmail.com>
2015-09-24 18:04:10 -0500

    updated budgeting

--- app/static/angular/budgeting/budgeting.js
+++ app/static/angular/budgeting/budgeting.js
@@ -8,12 +8,28 @@ angular.module('app.budgeting', ['smart-table'])
     })
 })
 .controller('BudgetingController', function($scope, store, $state) {
-    $scope.username = store.get('username');
+  // Bootstrap tooltip init
+  $('[data-toggle="tooltip"]').tooltip();
 
-    $scope.logOut = function() {
-        store.remove('jwt');
-        $state.go('login');
-    }
+  // Signed in user
+  $scope.username = store.get('username');
+  // Load user's projects
+  getProjects();
+
+  // Log Out function
+  $scope.logOut = function() {
+      store.remove('jwt');
+      $state.go('login');
+  }
+  // GET PROJECTS function
+  function getCategories() {
+      $http.get('/api/categories?q={"filters":[{"name":"project_id","op":"equals","val":"' + store.get('project_id') + '"}]}')
+      .then(function(response) {
+          $scope.categories_list = response.data.objects;
+      }, function(response) {
+          // Could not load user's project
+      });
+  }
 
 });
 
