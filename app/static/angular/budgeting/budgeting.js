2ff4732c6bb3ea97974c5b1750a992c1a7fb35a2
amylopes <alopes5982@gmail.com>
2015-09-24 19:22:22 -0500

    add items

--- app/static/angular/budgeting/budgeting.js
+++ app/static/angular/budgeting/budgeting.js
@@ -8,7 +8,7 @@ angular.module('app.budgeting', [
         controller: 'BudgetingController'
     })
 })
-.controller('BudgetingController', function($scope, store, $state) {
+.controller('BudgetingController', function($scope, store, $state, $http) {
   // Bootstrap tooltip init
   $('[data-toggle="tooltip"]').tooltip();
 
@@ -17,14 +17,25 @@ angular.module('app.budgeting', [
   $scope.username = store.get('username');
   // Load user's projects
   getCategories();
-
+  getItems();
   // Log Out function
   $scope.logOut = function() {
       store.remove('jwt');
       $state.go('login');
   }
-  // GET PROJECTS function
+  // GET CATEGORIES function
   function getCategories() {
+      console.log('im in getcategories');
+      $http.get('/api/categories?q={"filters":[{"name":"project_id","op":"equals","val":"' + store.get('project_id') + '"}]}')
+      .then(function(response) {
+          console.log(response.data);
+          $scope.categories_list = response.data.objects;
+      }, function(response) {
+          // Could not load user's project
+      });
+  }
+  // GET ITEMS function
+  function getItems() {
       console.log('im in getcategories');
       $http.get('/api/categories?q={"filters":[{"name":"project_id","op":"equals","val":"' + store.get('project_id') + '"}]}')
       .then(function(response) {
