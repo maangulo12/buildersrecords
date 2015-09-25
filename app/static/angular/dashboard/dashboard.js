3480e7102b23f8dae50dd45d1569fca48061817c
Miguel Angulo <maangulo12@gmail.com>
2015-09-25 13:02:43 -0500

    Update dashboard module

--- app/static/angular/dashboard/dashboard.js
+++ app/static/angular/dashboard/dashboard.js
@@ -1,17 +1,19 @@
-angular.module('app.dashboard', [
+angular.module('app.dashboard', [])
 
-])
 .config(function($stateProvider) {
     $stateProvider.state('dashboard', {
         url: '/dashboard',
         templateUrl: 'angular/dashboard/dashboard.html',
         controller: 'DashboardController'
-    })
+    });
 })
 .controller('DashboardController', function($scope, store, $state) {
-    $scope.username = store.get('username');
-    $scope.project_name = store.get('project_name');
+    init();
 
+    function init() {
+        $scope.username = store.get('username');
+        $scope.project_name = store.get('project_name');
+    }
     $scope.logOut = function() {
         store.remove('jwt');
         $state.go('login');
