929c2753d056cc26a5defe47f5578b37168c4211
Miguel Angulo <maangulo12@gmail.com>
2015-09-25 13:03:03 -0500

    Update expenditures module

--- app/static/angular/expenditures/expenditures.js
+++ app/static/angular/expenditures/expenditures.js
@@ -1,31 +1,57 @@
-angular.module('app.expenditures', [
+angular.module('app.expenditures', [])
 
-])
 .config(function($stateProvider) {
     $stateProvider.state('expenditures', {
         url: '/expenditures',
         templateUrl: 'angular/expenditures/expenditures.html',
         controller: 'ExpendituresController'
-    })
+    });
 })
 .controller('ExpendituresController', function($scope, store, $state, $http) {
-    $scope.username = store.get('username');
-    getExpenditures();
+    init();
 
-    $scope.logOut = function() {
-        store.remove('jwt');
-        $state.go('login');
+    function init() {
+        $('#delete_button').addClass('disabled');
+        $scope.username = store.get('username');
+        getExpenditures();
     }
-    $scope.clickedCheckbox = function() {
-
-    }
-    // GET EXPENDITURES function
+    // GET function
     function getExpenditures() {
         $http.get('/api/expenditures?q={"filters":[{"name":"project_id","op":"equals","val":"' + store.get('project_id') + '"}]}')
         .then(function(response) {
             $scope.expenditure_list = response.data.objects;
-        }, function(response) {
+        }, function(error) {
             // Could not load user's project
+        });
+    }
+    $scope.logOut = function() {
+        store.remove('jwt');
+        $state.go('login');
+    }
+    $scope.clickedAllCheckbox = function() {
+        
+    }
+    // ADD functions
+    $scope.showAddExpenditureModal = function() {
+        $scope.date = '';
+        $scope.add_expenditure_form.$setPristine();
+        $('#add_expenditure_modal').modal('show');
+    }
+    $scope.addExpenditure = function() {
+        $http.post('/api/expenditures', {
+            date: $scope.date,
+            vendor: 'Vendor Name',
+            description: 'Description',
+            notes: 'Notes',
+            loan: true,
+            project_id: store.get('project_id')
+        })
+        .then(function(response) {
+            $('#add_expenditure_modal').modal('hide');
+            getExpenditures();
+        }, function(error) {
+            $scope.add_expenditure_form.$invalid = true;
+            $scope.add_expenditure_form.date.$invalid = true;
         });
     }
 });
