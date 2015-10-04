35ab9f2969c7a811080dc80a0fe9613b169079cf
Miguel Angulo <maangulo12@gmail.com>
2015-10-03 22:58:56 -0500

    Add format directive
    Use format for numeric fields

--- app/static/angular/directives.js
+++ app/static/angular/directives.js
@@ -81,4 +81,22 @@ angular.module('app.directives', [
             }
         }
     }
+})
+.directive('format', function ($filter) {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attrs, ctrl) {
+            if (!ctrl) {
+                return;
+            }
+            ctrl.$formatters.unshift(function (a) {
+                return $filter(attrs.format)(ctrl.$modelValue)
+            });
+            ctrl.$parsers.unshift(function (viewValue) {
+                var plainNumber = viewValue.replace(/[^\d|\-+|\.+]/g, '');
+                element.val($filter(attrs.format)(plainNumber));
+                return plainNumber;
+            });
+        }
+    };
 });
