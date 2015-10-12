3b43771415dc627d94877bf8210912a772a93191
Miguel Angulo <maangulo12@gmail.com>
2015-10-12 16:50:15 -0500

    Fix dashboard js

--- app/static/angular/dashboard/dashboard.js
+++ app/static/angular/dashboard/dashboard.js
@@ -12,107 +12,59 @@ angular.module('app.dashboard', [])
 })
 .controller('DashboardController', function($scope, store, $state) {
     init();
-    drawBudgetPieChart();
-    drawExpendituresPieChart();
+    // drawBudgetPieChart();
 
     function init() {
         $scope.username = store.get('username');
         $scope.project_name = store.get('project').name;
     }
-    function drawBudgetPieChart() {
-        var data = [
-            {
-                value: 300,
-                color:"#F7464A",
-                highlight: "#FF5A5E",
-                label: "Red"
-            },
-            {
-                value: 50,
-                color: "#46BFBD",
-                highlight: "#5AD3D1",
-                label: "Green"
-            },
-            {
-                value: 100,
-                color: "#FDB45C",
-                highlight: "#FFC870",
-                label: "Yellow"
-            }
-        ];
-        var options = {
-                responsive : true,
-                labelFontSize : '18',
-                tooltipTemplate : "<%if (label){%><%=label%>: <%}%>$<%= value.formatMoney(0, '.', ',') %>",
-                legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><div class=\"comm-how\">$<%=segments[i].value.formatMoney(0, '.', ',')%></div><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
-        };
-        var ctx = $('#modular-doughnut').get(0).getContext('2d');
-        var chart = new Chart(ctx).Doughnut(data, options);
-        var legend = document.createElement('div');
-		legend.innerHTML = chart.generateLegend();
-
-        var helpers = Chart.helpers;
-		helpers.each(legend.firstChild.childNodes, function(legendNode, index) {
-			helpers.addEvent(legendNode, 'mouseover', function() {
-				var activeSegment = chart.segments[index];
-				activeSegment.save();
-				activeSegment.fillColor = activeSegment.highlightColor;
-				chart.showTooltip([activeSegment]);
-				activeSegment.restore();
-			});
-		});
-		helpers.addEvent(legend.firstChild, 'mouseout', function() {
-			chart.draw();
-		});
-		document.getElementById('legend-holder').appendChild(legend.firstChild);
-    }
-    function drawExpendituresPieChart() {
-        var data = [
-            {
-                value: 300,
-                color:"#F7464A",
-                highlight: "#FF5A5E",
-                label: "Red"
-            },
-            {
-                value: 50,
-                color: "#46BFBD",
-                highlight: "#5AD3D1",
-                label: "Green"
-            },
-            {
-                value: 100,
-                color: "#FDB45C",
-                highlight: "#FFC870",
-                label: "Yellow"
-            }
-        ];
-        var options = {
-                responsive : true,
-                labelFontSize : '18',
-                tooltipTemplate : "<%if (label){%><%=label%>: <%}%>$<%= value.formatMoney(0, '.', ',') %>",
-                legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><div class=\"comm-how\">$<%=segments[i].value.formatMoney(0, '.', ',')%></div><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
-        };
-        var ctx = $('#modular-doughnut2').get(0).getContext('2d');
-        var chart = new Chart(ctx).Doughnut(data, options);
-        var legend = document.createElement('div');
-		legend.innerHTML = chart.generateLegend();
-
-        var helpers = Chart.helpers;
-		helpers.each(legend.firstChild.childNodes, function(legendNode, index) {
-			helpers.addEvent(legendNode, 'mouseover', function() {
-				var activeSegment = chart.segments[index];
-				activeSegment.save();
-				activeSegment.fillColor = activeSegment.highlightColor;
-				chart.showTooltip([activeSegment]);
-				activeSegment.restore();
-			});
-		});
-		helpers.addEvent(legend.firstChild, 'mouseout', function() {
-			chart.draw();
-		});
-		document.getElementById('legend-holder2').appendChild(legend.firstChild);
-    }
+    // function drawBudgetPieChart() {
+    //     var data = [
+    //         {
+    //             value: 300,
+    //             color:"#F7464A",
+    //             highlight: "#FF5A5E",
+    //             label: "Red"
+    //         },
+    //         {
+    //             value: 50,
+    //             color: "#46BFBD",
+    //             highlight: "#5AD3D1",
+    //             label: "Green"
+    //         },
+    //         {
+    //             value: 100,
+    //             color: "#FDB45C",
+    //             highlight: "#FFC870",
+    //             label: "Yellow"
+    //         }
+    //     ];
+    //     var options = {
+    //             responsive : true,
+    //             labelFontSize : '18',
+    //             tooltipTemplate : "<%if (label){%><%=label%>: <%}%>$<%= value.formatMoney(0, '.', ',') %>",
+    //             legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><div class=\"comm-how\">$<%=segments[i].value.formatMoney(0, '.', ',')%></div><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
+    //     };
+    //     var ctx = $('#modular-doughnut').get(0).getContext('2d');
+    //     var chart = new Chart(ctx).Doughnut(data, options);
+    //     var legend = document.createElement('div');
+	// 	legend.innerHTML = chart.generateLegend();
+    //
+    //     var helpers = Chart.helpers;
+	// 	helpers.each(legend.firstChild.childNodes, function(legendNode, index) {
+	// 		helpers.addEvent(legendNode, 'mouseover', function() {
+	// 			var activeSegment = chart.segments[index];
+	// 			activeSegment.save();
+	// 			activeSegment.fillColor = activeSegment.highlightColor;
+	// 			chart.showTooltip([activeSegment]);
+	// 			activeSegment.restore();
+	// 		});
+	// 	});
+	// 	helpers.addEvent(legend.firstChild, 'mouseout', function() {
+	// 		chart.draw();
+	// 	});
+	// 	document.getElementById('legend-holder').appendChild(legend.firstChild);
+    // }
     $scope.logOut = function() {
         store.remove('jwt');
         $state.go('login');
