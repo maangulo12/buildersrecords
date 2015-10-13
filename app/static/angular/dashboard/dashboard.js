c8bf1737ae4a3f70d57a92427004af061acf5d40
Miguel Angulo <maangulo12@gmail.com>
2015-10-13 00:19:31 -0500

    Update dashboard module

--- app/static/angular/dashboard/dashboard.js
+++ app/static/angular/dashboard/dashboard.js
@@ -10,63 +10,82 @@ angular.module('app.dashboard', [])
         }
     });
 })
-.controller('DashboardController', function($scope, store, $state) {
+.controller('DashboardController', function($scope, store, $state, $http) {
     init();
-    // drawBudgetPieChart();
 
     function init() {
         $scope.username = store.get('username');
         $scope.project_name = store.get('project').name;
+        drawPieCharts();
     }
-    // function drawBudgetPieChart() {
-    //     var data = [
-    //         {
-    //             value: 300,
-    //             color:"#F7464A",
-    //             highlight: "#FF5A5E",
-    //             label: "Red"
-    //         },
-    //         {
-    //             value: 50,
-    //             color: "#46BFBD",
-    //             highlight: "#5AD3D1",
-    //             label: "Green"
-    //         },
-    //         {
-    //             value: 100,
-    //             color: "#FDB45C",
-    //             highlight: "#FFC870",
-    //             label: "Yellow"
-    //         }
-    //     ];
-    //     var options = {
-    //             responsive : true,
-    //             labelFontSize : '18',
-    //             tooltipTemplate : "<%if (label){%><%=label%>: <%}%>$<%= value.formatMoney(0, '.', ',') %>",
-    //             legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><div class=\"comm-how\">$<%=segments[i].value.formatMoney(0, '.', ',')%></div><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
-    //     };
-    //     var ctx = $('#modular-doughnut').get(0).getContext('2d');
-    //     var chart = new Chart(ctx).Doughnut(data, options);
-    //     var legend = document.createElement('div');
-	// 	legend.innerHTML = chart.generateLegend();
-    //
-    //     var helpers = Chart.helpers;
-	// 	helpers.each(legend.firstChild.childNodes, function(legendNode, index) {
-	// 		helpers.addEvent(legendNode, 'mouseover', function() {
-	// 			var activeSegment = chart.segments[index];
-	// 			activeSegment.save();
-	// 			activeSegment.fillColor = activeSegment.highlightColor;
-	// 			chart.showTooltip([activeSegment]);
-	// 			activeSegment.restore();
-	// 		});
-	// 	});
-	// 	helpers.addEvent(legend.firstChild, 'mouseout', function() {
-	// 		chart.draw();
-	// 	});
-	// 	document.getElementById('legend-holder').appendChild(legend.firstChild);
-    // }
+    function drawPieCharts() {
+        $http.get('/api/categories?q={"filters":[{"name":"project_id","op":"equals","val":"' + store.get('project').id + '"}]}')
+        .then(function(response) {
+            $scope.category_list = response.data.objects;
+
+            var i = 0;
+
+            var budget_data = [];
+            var budget_colors = getColorList($scope.category_list.length, 'mediumspringgreen', 'darkslategray');
+
+            var expenditures_data = [];
+            var expenditures_colors = getColorList($scope.category_list.length, 'crimson', 'brown');
+
+            angular.forEach($scope.category_list, function(category) {
+
+                var category_cost = 0;
+                angular.forEach(category.items, function(item) {
+                    category_cost += item.amount;
+                });
+
+                budget_data.push({
+                    value: category_cost,
+                    color: budget_colors[i],
+                    highlight: budget_colors[i],
+                    label: category.name
+                });
+
+                var category_expenditure = 0;
+                angular.forEach(category.expenditures, function(expenditure) {
+                    category_expenditure += expenditure.amount;
+                });
+
+                expenditures_data.push({
+                    value: category_expenditure,
+                    color: expenditures_colors[i],
+                    highlight: expenditures_colors[i],
+                    label: category.name
+                });
+
+                i++;
+            });
+
+            var ctx = $('#modular-doughnut').get(0).getContext('2d');
+            var chart = new Chart(ctx).Doughnut(budget_data, {
+                responsive: true
+            });
+
+            var ctx2 = $('#modular-doughnut2').get(0).getContext('2d');
+            var chart2 = new Chart(ctx2).Doughnut(expenditures_data, {
+                responsive: true
+            });
+        }, function(error) {
+            $scope.error_msg = 'Could not load your budget pie chart.';
+        });
+    }
     $scope.logOut = function() {
         store.remove('jwt');
         $state.go('login');
+    }
+    function getColorList(num_items, start_spec, end_spec) {
+        var colors = [];
+        var rainbow = new Rainbow();
+        rainbow.setNumberRange(1, num_items);
+        rainbow.setSpectrum(start_spec, end_spec);
+
+        for (var i = 1; i <= num_items; i++) {
+            colors.push('#' + rainbow.colourAt(i));
+        }
+        return colors;
     }
 });
