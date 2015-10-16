c6cb52608bcf6a134d95d61a3941338e03e41045
Miguel Angulo <maangulo12@gmail.com>
2015-10-15 22:40:15 -0500

    Update dashboard module

--- app/static/angular/dashboard/dashboard.js
+++ app/static/angular/dashboard/dashboard.js
@@ -118,5 +118,5 @@ angular.module('app.dashboard', [])
             i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
             j = (j = i.length) > 3 ? j % 3 : 0;
         return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
-    };
+    }
 });
