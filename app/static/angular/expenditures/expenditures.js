96aba5f840f082687a271378cbb7a8a975cacafd
Miguel Angulo <maangulo12@gmail.com>
2015-10-15 22:40:46 -0500

    Update expenditure module

--- app/static/angular/expenditures/expenditures.js
+++ app/static/angular/expenditures/expenditures.js
@@ -80,6 +80,35 @@ angular.module('app.expenditures', [])
         $http.get('/api/categories?q={"filters":[{"name":"project_id","op":"equals","val":"' + store.get('project').id + '"}]}')
         .then(function(response) {
             $scope.category_list = response.data.objects;
+
+            var i = 0;
+            var expenditures_data = [];
+            var expenditures_colors = getColorList($scope.category_list.length, 'coral', 'darkred');
+
+            angular.forEach($scope.category_list, function(category) {
+                var total_expenditure = 0;
+                angular.forEach(category.expenditures, function(expenditure) {
+                    total_expenditure += expenditure.amount;
+                });
+                expenditures_data.push({
+                    value: total_expenditure,
+                    color: expenditures_colors[i],
+                    highlight: expenditures_colors[i],
+                    label: category.name
+                });
+                i++;
+            });
+            // Draw Expenditures Pie Chart
+            var ctx = $('#modular-doughnut').get(0).getContext('2d');
+            var chart = new Chart(ctx).Doughnut(expenditures_data, {
+                responsive: true,
+                tooltipTemplate: "<%if (label){%><%=label%>: <%}%>$<%= value.formatMoney(0, '.', ',') %>",
+                legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><div class=\"comm-how\">$<%=segments[i].value.formatMoney(0, '.', ',')%></div><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
+            });
+            var legend = document.createElement('div');
+    		legend.innerHTML = chart.generateLegend();
+            document.getElementById('legend-holder').appendChild(legend.firstChild);
+
         }, function(error) {
             $scope.error_msg = 'Could not load your project\'s categories. Please try to refresh the page.';
         });
@@ -172,5 +201,26 @@ angular.module('app.expenditures', [])
         }, function(error) {
             $scope.edit_expenditure_form.$invalid = true;
         });
+    }
+    function getColorList(num_items, start_spec, end_spec) {
+        var colors = [];
+        var rainbow = new Rainbow();
+        rainbow.setNumberRange(1, num_items);
+        rainbow.setSpectrum(start_spec, end_spec);
+
+        for (var i = 1; i <= num_items; i++) {
+            colors.push('#' + rainbow.colourAt(i));
+        }
+        return colors;
+    }
+    Number.prototype.formatMoney = function(c, d, t) {
+        var n = this,
+            c = isNaN(c = Math.abs(c)) ? 2 : c,
+            d = d == undefined ? "." : d,
+            t = t == undefined ? "," : t,
+            s = n < 0 ? "-" : "",
+            i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
+            j = (j = i.length) > 3 ? j % 3 : 0;
+        return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
     }
 });
