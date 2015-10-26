8a4c337425f4f3ef52489f26d74da97fecafee23
Miguel Angulo <maangulo12@gmail.com>
2015-10-26 14:58:35 -0500

    Update API
    Add subcontractors api

--- app/api.py
+++ app/api.py
@@ -8,20 +8,21 @@
     This is the API module for the Flask-Restless extension.
 
     Current APIs:
-        -Users        : /api/users
-        -Projects     : /api/projects
-        -Categories   : /api/categories
-        -Items        : /api/items
-        -Expenditures : /api/expenditures
-        -Funds        : /api/funds
-        -Draws        : /api/draws
+        -Users          : /api/users
+        -Projects       : /api/projects
+        -Categories     : /api/categories
+        -Items          : /api/items
+        -Expenditures   : /api/expenditures
+        -Funds          : /api/funds
+        -Draws          : /api/draws
+        -Subcontractors : /api/subcontractors
 
     *Note: verify_jwt is imported in this module in order to protect API.
 """
 
 from app import api_manager
 from app.jwt import verify_jwt
-from app.models import User, Project, Category, Item, Expenditure, Fund, Draw
+from app.models import User, Project, Category, Item, Expenditure, Fund, Draw, Subcontractor
 
 
 # Users: /api/users
@@ -142,3 +143,23 @@ api_manager.create_api(Draw,
                                           DELETE_SINGLE=[verify_jwt],
                                           DELETE_MANY=[verify_jwt]),
                        collection_name='draws')
+
+
+# Subcontractors: /api/subcontractors
+# methods allowed: GET, POST, DELETE, PUT
+# results_per_page: pagination turned off (set to 0)
+# allow_delete_many: to allow bulk deletions
+# All methods are protected (all of them are listed in preprocessors)
+api_manager.create_api(Subcontractor,
+                       methods=['GET', 'POST', 'DELETE', 'PUT'],
+                       url_prefix='/api',
+                       results_per_page=0,
+                       allow_delete_many=True,
+                       preprocessors=dict(POST=[verify_jwt],
+                                          GET_SINGLE=[verify_jwt],
+                                          GET_MANY=[verify_jwt],
+                                          PUT_SINGLE=[verify_jwt],
+                                          PUT_MANY=[verify_jwt],
+                                          DELETE_SINGLE=[verify_jwt],
+                                          DELETE_MANY=[verify_jwt]),
+                       collection_name='subcontractors')
