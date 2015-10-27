09ff56404a3c7500f428281c748dfc197941eb47
Miguel Angulo <maangulo12@gmail.com>
2015-10-26 22:37:48 -0500

    Update subcontractors module

--- app/static/angular/subcontractors/subcontractors.js
+++ app/static/angular/subcontractors/subcontractors.js
@@ -10,16 +10,25 @@ angular.module('app.subcontractors', [])
         }
     });
 })
-.controller('SubcontractorController', function($scope, store, $state) {
+.controller('SubcontractorController', function($scope, store, $state, $http) {
     init();
 
     // Init function
     function init() {
         $scope.username = store.get('username');
+        getSubcontractors();
     }
     // Logout function
     $scope.logOut = function() {
         store.remove('jwt');
         $state.go('login');
+    }
+    function getSubcontractors() {
+        $http.get('/api/subcontractors?q={"filters":[{"name":"project_id","op":"equals","val":"' + store.get('project').id + '"}]}')
+        .then(function(response) {
+            $scope.subcontractor_list = response.data.objects;
+        }, function(error) {
+            $scope.error_msg = 'Could not load your subcontractor list. Please try to refresh the page.';
+        });
     }
 });
