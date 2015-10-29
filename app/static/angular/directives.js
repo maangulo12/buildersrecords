afb98dbd55df0a9c3a9a735a377a93616123e125
Miguel Angulo <maangulo12@gmail.com>
2015-10-29 01:32:06 -0500

    Add fileread directive

--- app/static/angular/directives.js
+++ app/static/angular/directives.js
@@ -85,4 +85,23 @@ angular.module('app.directives', [])
             });
         }
     }
+})
+.directive('fileread', function () {
+    // This directive is used uploading files
+    return {
+        scope: {
+            fileread: '='
+        },
+        link: function (scope, element, attrs) {
+            element.bind('change', function (changeEvent) {
+                var reader = new FileReader();
+                reader.onload = function (loadEvent) {
+                    scope.$apply(function () {
+                        scope.fileread = loadEvent.target.result;
+                    });
+                }
+                reader.readAsDataURL(changeEvent.target.files[0]);
+            });
+        }
+    }
 });
