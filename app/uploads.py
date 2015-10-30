4decb7a3edb0338538bcc0e0504770dacce134b1
Miguel Angulo <maangulo12@gmail.com>
2015-10-30 11:16:56 -0500

    Update uploads module

--- app/uploads.py
+++ app/uploads.py
@@ -14,20 +14,33 @@
 from flask import make_response, request
 
 from app import app
+from app.utility import allowed_file, upload_file, check_file
 
 
 API_ENTRY = '/api/upload'
 
 @app.route(API_ENTRY + '/ubuildit', methods=['POST'])
 def ubuildit():
+    """
+    This route is used for uploading a UBuildIt Cost Review excel file.
+    """
     data = request.get_json(force=True)
+    filename = data.get('filename', None)
     temp_file = data.get('file', None)
     user_id = data.get('user_id', None)
-    criterion = [temp_file, user_id, len(data) == 2]
+    criterion = [filename, temp_file, user_id, len(data) == 3]
 
     if not all(criterion):
-        return make_response('Could not upload file', 400)
+        return make_response('Bad request', 400)
 
-    #if upload_file(temp_file) and check_file(temp_file):
-    #    parsed_file = parse_file(temp_file)
-    return make_response('File was successfully uploaded', 201)
+    if allowed_file(filename):
+        if upload_file(filename, temp_file):
+            if check_file(filename):
+                # parse file
+                return make_response('File was successfully uploaded', 201)
+            else:
+                return make_response('Different excel file', 400)
+        else:
+            return make_response('Failed to upload file', 400)
+    else:
+        return make_response('Wrong file extension', 400)
