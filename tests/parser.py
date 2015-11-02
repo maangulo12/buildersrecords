71f1711445330696093e0d62867bd7adc46433d1
Miguel Angulo <maangulo12@gmail.com>
2015-11-02 01:15:35 -0600

    Update parser module

--- tests/parser.py
+++ tests/parser.py
@@ -15,12 +15,10 @@ from xlrd import open_workbook, xldate_as_tuple
 def parse_invoice_file(path):
     """
     Parses the Invoice Excel file.
-    :param file: The location path of the file
+    :param path: the location of the file
     """
-
     wb = open_workbook(filename=path)
     ws = wb.sheet_by_name('Invoices indexed by date')
-
     expenditure_list = []
 
     for i in range(2, 140):
@@ -38,47 +36,37 @@ def parse_invoice_file(path):
             'cost':        convert_to_float(cells[3].value),
             'notes':       cells[4].value
         })
-
     return expenditure_list
 
 
 def parse_ubuildit_file(path):
     """
     Parses the UBuildIt Excel file.
-    :param file: The location path of the file
+    :param path: the location of the file
     :returns
         category_list: {
             'category_name:' 'name_of_category',
             'item_list': 'list_of_items[]'
         }
     """
-
     wb = open_workbook(filename=path)
     ws = wb.sheet_by_name('UBI Cost Review')
 
     category_list = []
-    category_list.append({'category_name': ws.cell_value(
-        5, 2), 'item_list': get_item_list(ws, 6, 15)})
-    category_list.append({'category_name': ws.cell_value(
-        16, 2), 'item_list': get_item_list(ws, 17, 33)})
-    category_list.append({'category_name': ws.cell_value(
-        34, 2), 'item_list': get_item_list(ws, 35, 43)})
-    category_list.append({'category_name': ws.cell_value(
-        44, 2), 'item_list': get_item_list(ws, 45, 47)})
-    category_list.append({'category_name': ws.cell_value(
-        48, 2), 'item_list': get_item_list(ws, 49, 52)})
-    category_list.append({'category_name': ws.cell_value(
-        53, 2), 'item_list': get_item_list(ws, 54, 60)})
-    category_list.append({'category_name': ws.cell_value(
-        61, 2), 'item_list': get_item_list(ws, 62, 93)})
-    category_list.append({'category_name': ws.cell_value(
-        94, 2), 'item_list': get_item_list(ws, 95, 130)})
+    category_list.append({'category_name': ws.cell_value(5, 2), 'item_list': get_item_list(ws, 6, 15)})
+    category_list.append({'category_name': ws.cell_value(16, 2), 'item_list': get_item_list(ws, 17, 33)})
+    category_list.append({'category_name': ws.cell_value(34, 2), 'item_list': get_item_list(ws, 35, 43)})
+    category_list.append({'category_name': ws.cell_value(44, 2), 'item_list': get_item_list(ws, 45, 47)})
+    category_list.append({'category_name': ws.cell_value(48, 2), 'item_list': get_item_list(ws, 49, 52)})
+    category_list.append({'category_name': ws.cell_value(53, 2), 'item_list': get_item_list(ws, 54, 60)})
+    category_list.append({'category_name': ws.cell_value(61, 2), 'item_list': get_item_list(ws, 62, 93)})
+    category_list.append({'category_name': ws.cell_value(94, 2), 'item_list': get_item_list(ws, 95, 130)})
     return category_list
 
 
 def get_item_list(ws, start, end):
     """
-    Get item list for parsing excel file.
+    Get item list from excel file.
     :param start: start position to read row of file
     :param end: end position to read row of file
     :return: Returns the item_list parsed
@@ -89,7 +77,6 @@ def get_item_list(ws, start, end):
         cells = ws.row_slice(rowx=i,
                              start_colx=2,
                              end_colx=10)
-
         temp_list.append({
             'cost_category':     cells[0].value,
             'description':       cells[1].value,
@@ -105,7 +92,7 @@ def get_item_list(ws, start, end):
 
 def convert_to_float(s):
     """
-    Used for converting a String into int or float.
+    Converts a String into int or float.
     :param s: String
     :return: Returns an int or float
     """
