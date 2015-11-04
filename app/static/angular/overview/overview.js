66bc2d7d9f3d86e4e00bae9694eb84382ee30bf5
Miguel Angulo <maangulo12@gmail.com>
2015-11-04 17:30:25 -0600

    Update overview module

--- app/static/angular/overview/overview.js
+++ app/static/angular/overview/overview.js
@@ -23,11 +23,6 @@ angular.module('app.overview', [])
         .then(function(response) {
             $scope.category_list = response.data.objects;
 
-            var data = {};
-            var labels = [];
-            var estimated = [];
-            var actual = [];
-            var paid = [];
             var grand_total_budget = 0;
             var grand_total_actual = 0;
             var grand_total_expenditure = 0;
@@ -58,53 +53,11 @@ angular.module('app.overview', [])
                     category.paid = Math.round(total_expenditure / total_actual * 100);
                     category.unpaid = total_actual - total_expenditure;
                 }
-                labels.push(category.name);
-                estimated.push(total_budget);
-                actual.push(total_actual);
-                paid.push(total_expenditure);
             });
-
             $scope.grand_total_budget = grand_total_budget;
             $scope.grand_total_actual = grand_total_actual;
             $scope.grand_total_expenditure = grand_total_expenditure;
 
-            data = {
-                labels: labels,
-                datasets: [
-                    {
-                        label: 'Estimated Budget',
-                        fillColor: "rgba(220, 220, 220, 0.5)",
-                        strokeColor: "rgba(220, 220, 220, 0.8)",
-                        highlightFill: "rgba(220, 220, 220, 0.75)",
-                        highlightStroke: "rgba(220, 220, 220, 1)",
-                        data: estimated
-                    },
-                    {
-                        label: 'Actual Job Cost',
-                        fillColor: "rgba(151, 187, 205, 0.5)",
-                        strokeColor: "rgba(151, 187, 205, 0.8)",
-                        highlightFill: "rgba(151, 187, 205, 0.75)",
-                        highlightStroke: "rgba(151, 187, 205, 1)",
-                        data: actual
-                    },
-                    {
-                        label: 'Paid',
-                        fillColor: "rgba(90, 198, 127, 0.5)",
-                        strokeColor: "rgba(90, 198, 127, 0.8)",
-                        highlightFill: "rgba(90, 198, 127, 0.75)",
-                        highlightStroke: "rgba(90, 198, 127, 1)",
-                        data: paid
-                    }
-                ]
-            };
-            // Draw Bar Chart
-            var ctx = $('#modular-bar').get(0).getContext('2d');
-            var chart = new Chart(ctx).Bar(data, {
-                scaleFontSize: 0,
-                barValueSpacing: 15,
-                responsive: true,
-                multiTooltipTemplate: "<%if (label){%><%=label%>: <%}%>$<%= value.formatMoney(2, '.', ',') %>"
-            });
         }, function(error) {
             $scope.error_msg = 'Could not load your cost review table.';
         });
