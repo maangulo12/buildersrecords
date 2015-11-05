390d888e2265f0d6cf8eee8ba7cb27dfd42290ba
Miguel Angulo <maangulo12@gmail.com>
2015-11-05 14:37:27 -0600

    Update sample data module

--- tests/sample_data.py
+++ tests/sample_data.py
@@ -27,11 +27,11 @@ def populate_db(app):
 
     # Create User
     client.post('/api/users', data=json.dumps({
-        'username': 'test',
-        'password': 'test',
+        'username':   'test',
+        'password':   'test',
         'first_name': 'test',
-        'last_name': 'test',
-        'email': 'test@gmail.com'
+        'last_name':  'test',
+        'email':      'test@gmail.com'
     }),
         headers={
         'Content-Type': 'application/json'
@@ -39,7 +39,7 @@ def populate_db(app):
 
     # Authenticate User
     response = client.post('/api/auth', data=json.dumps({
-        'login': 'test',
+        'login':    'test',
         'password': 'test'
     }),
         headers={
@@ -52,16 +52,16 @@ def populate_db(app):
 
     # Create Project
     client.post('/api/projects', data=json.dumps({
-        'name': 'UBuildIt - Tim & Maritza Messer',
-        'address': '251 Wizard Way',
-        'city': 'Spring Branch',
-        'state': 'TX',
-        'zipcode': '78070',
+        'name':         'UBuildIt - Tim & Maritza Messer',
+        'address':      '251 Wizard Way',
+        'city':         'Spring Branch',
+        'state':        'TX',
+        'zipcode':      '78070',
         'project_type': 'ubuildit',
-        'user_id':  1
+        'user_id':      1
     }),
         headers={
-        'Content-Type': 'application/json',
+        'Content-Type':  'application/json',
         'Authorization': 'Bearer ' + token
     })
 
@@ -71,78 +71,78 @@ def populate_db(app):
     for category in data:
         # Add Category
         client.post('/api/categories', data=json.dumps({
-            'name': category['category_name'],
+            'name':       category['category_name'],
             'project_id': 1
         }),
             headers={
-            'Content-Type': 'application/json',
+            'Content-Type':  'application/json',
             'Authorization': 'Bearer ' + token
         })
 
         for item in category['item_list']:
             # Add Item
             client.post('/api/items', data=json.dumps({
-                'name': item['cost_category'],
+                'name':        item['cost_category'],
                 'description': item['description'],
-                'budget': item['budget'],
-                'actual': item['actual'],
-                'notes': item['explanations'],
+                'budget':      item['budget'],
+                'actual':      item['actual'],
+                'notes':       item['explanations'],
                 'category_id': data.index(category) + 1,
-                'project_id': 1
+                'project_id':  1
             }),
                 headers={
-                'Content-Type': 'application/json',
+                'Content-Type':  'application/json',
                 'Authorization': 'Bearer ' + token
             })
 
     # Add Funds/Loans
     client.post('/api/funds', data=json.dumps({
-        'name': 'Messer',
-        'loan': False,
-        'amount': 100000.00,
+        'name':       'Messer',
+        'loan':       False,
+        'amount':     100000.00,
         'project_id': 1
     }, use_decimal=True),
         headers={
-        'Content-Type': 'application/json',
+        'Content-Type':  'application/json',
         'Authorization': 'Bearer ' + token
     })
     client.post('/api/funds', data=json.dumps({
-        'name': 'Blanco Loan',
-        'loan': True,
-        'amount': 330000.00,
+        'name':       'Blanco Loan',
+        'loan':       True,
+        'amount':     330000.00,
         'project_id': 1
     }, use_decimal=True),
         headers={
-        'Content-Type': 'application/json',
+        'Content-Type':  'application/json',
         'Authorization': 'Bearer ' + token
     })
 
     # Add Draws
     client.post('/api/draws', data=json.dumps({
-        'date': '09/24/2015',
-        'amount': 25000.00,
+        'date':    '09/24/2015',
+        'amount':  25000.00,
         'fund_id': 2
     }, use_decimal=True),
         headers={
-        'Content-Type': 'application/json',
+        'Content-Type':  'application/json',
         'Authorization': 'Bearer ' + token
     })
     client.post('/api/draws', data=json.dumps({
-        'date': '10/01/2015',
-        'amount': 5000.00,
+        'date':    '10/01/2015',
+        'amount':  5000.00,
         'fund_id': 2
     }, use_decimal=True),
         headers={
-        'Content-Type': 'application/json',
+        'Content-Type':  'application/json',
         'Authorization': 'Bearer ' + token
     })
     client.post('/api/draws', data=json.dumps({
-        'date': '10/03/2015',
-        'amount': 7500.00,
+        'date':    '10/03/2015',
+        'amount':  7500.00,
         'fund_id': 2
     }, use_decimal=True),
         headers={
-        'Content-Type': 'application/json',
+        'Content-Type':  'application/json',
         'Authorization': 'Bearer ' + token
     })
 
@@ -154,68 +154,68 @@ def populate_db(app):
             fund_id = 2
 
         client.post('/api/expenditures', data=json.dumps({
-            'date': expenditure['date'],
-            'vendor': expenditure['vendor'],
-            'notes': expenditure['description'],
-            'cost': expenditure['cost'],
+            'date':        expenditure['date'],
+            'vendor':      expenditure['vendor'],
+            'notes':       expenditure['description'],
+            'cost':        expenditure['cost'],
             'category_id': random.randint(1, 8),
-            'item_id': random.randint(1, 110),
-            'fund_id': fund_id,
-            'project_id': 1
+            'item_id':     random.randint(1, 110),
+            'fund_id':     fund_id,
+            'project_id':  1
         }, use_decimal=True),
             headers={
-            'Content-Type': 'application/json',
+            'Content-Type':  'application/json',
             'Authorization': 'Bearer ' + token
         })
 
     # Add Subcontractors
     client.post('/api/subcontractors', data=json.dumps({
-        'name': 'John Smith',
-        'company': '84 Lumber',
+        'name':         'John Smith',
+        'company':      '84 Lumber',
         'contact_info': '210-543-4534',
-        'project_id': 1
+        'project_id':   1
     }),
         headers={
-        'Content-Type': 'application/json',
+        'Content-Type':  'application/json',
         'Authorization': 'Bearer ' + token
     })
     client.post('/api/subcontractors', data=json.dumps({
-        'name': 'Shawn Tarver',
-        'company': 'Ez Company',
+        'name':         'Shawn Tarver',
+        'company':      'Ez Company',
         'contact_info': '512-586-6516',
-        'project_id': 1
+        'project_id':   1
     }),
         headers={
-        'Content-Type': 'application/json',
+        'Content-Type':  'application/json',
         'Authorization': 'Bearer ' + token
     })
     client.post('/api/subcontractors', data=json.dumps({
-        'name': 'Mike Jones',
-        'company': 'Coca Cola',
+        'name':         'Mike Jones',
+        'company':      'Coca Cola',
         'contact_info': '210-253-5861',
-        'project_id': 1
+        'project_id':   1
     }),
         headers={
-        'Content-Type': 'application/json',
+        'Content-Type':  'application/json',
         'Authorization': 'Bearer ' + token
     })
     client.post('/api/subcontractors', data=json.dumps({
-        'name': 'Alex Ramirez',
-        'company': 'Rusty Corp',
+        'name':         'Alex Ramirez',
+        'company':      'Rusty Corp',
         'contact_info': '256-645-7854',
-        'project_id': 1
+        'project_id':   1
     }),
         headers={
-        'Content-Type': 'application/json',
+        'Content-Type':  'application/json',
         'Authorization': 'Bearer ' + token
     })
     client.post('/api/subcontractors', data=json.dumps({
-        'name': 'Nick Ross',
-        'company': 'AAA Company',
+        'name':         'Nick Ross',
+        'company':      'AAA Company',
         'contact_info': '123-454-3443',
-        'project_id': 1
+        'project_id':   1
     }),
         headers={
-        'Content-Type': 'application/json',
+        'Content-Type':  'application/json',
         'Authorization': 'Bearer ' + token
     })
