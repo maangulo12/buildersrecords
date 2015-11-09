034a7e1ffa8b1c0c90a496636463722a2a3e34ab
Miguel Angulo <maangulo12@gmail.com>
2015-11-09 15:58:13 -0600

    Update projects module

--- app/static/angular/projects/projects.js
+++ app/static/angular/projects/projects.js
@@ -2,24 +2,20 @@ angular.module('app.projects', [])
 
 .config(function($stateProvider) {
     $stateProvider.state('projects', {
-        url: '/projects',
+        url:         '/projects',
         templateUrl: 'angular/projects/projects.html',
-        controller: 'ProjectsController',
-        data: {
+        controller:  'ProjectsController',
+        data:        {
             requiresLogin: true
         }
     });
 })
-.controller('ProjectsController', function($scope, store, $state, $http) {
+.controller('ProjectsController', function($scope, store, ProjectService, UploadService) {
     init();
 
     function init() {
         $scope.username = store.get('user').username;
         getProjects();
-    }
-    $scope.logOut = function() {
-        store.remove('jwt');
-        $state.go('login');
     }
     $scope.clickedProject = function(project) {
         var index = $scope.project_list.indexOf(project);
@@ -29,16 +25,13 @@ angular.module('app.projects', [])
         }
         return false;
     }
-    $scope.redirectToOverview = function() {
-        $state.go('overview');
-    }
     // GET function
     function getProjects() {
-        $http.get('/api/projects?q={"filters":[{"name":"user_id","op":"equals","val":"' + store.get('user').id + '"}]}')
+        ProjectService.getProjects()
         .then(function(response) {
             $scope.project_list = response.data.objects;
         }, function(error) {
-            $scope.error_msg = 'Could not get your projects. Please try to refresh your page.';
+            $scope.error_msg_get = true;
         });
     }
     // ADD functions
@@ -47,94 +40,63 @@ angular.module('app.projects', [])
         $scope.new_project_form.$setPristine();
         $('#new_project_modal').modal('show');
     }
-    $scope.changeFile = function() {
-        console.log('File has been changed');
-    }
     $scope.createProject = function() {
         if ($('#project_file').length) {
             var file = $('#project_file')[0].files[0];
 
-            var fd = new FormData();
-            fd.append('file', file);
-            fd.append('name', $scope.project.name);
-            fd.append('address', $scope.project.address);
-            fd.append('city', $scope.project.city);
-            fd.append('state', $scope.project.state);
-            fd.append('zipcode', $scope.project.zipcode);
-            fd.append('home_sq', $scope.project.home_sq);
-            fd.append('project_type', $scope.project.type);
-            fd.append('username', store.get('user').username);
+            var form = new FormData();
+            form.append('file', file);
+            form.append('name', $scope.project.name);
+            form.append('address', $scope.project.address);
+            form.append('city', $scope.project.city);
+            form.append('state', $scope.project.state);
+            form.append('zipcode', $scope.project.zipcode);
+            form.append('home_sq', $scope.project.home_sq);
+            form.append('project_type', $scope.project.type);
+            form.append('username', store.get('user').username);
 
-            $http.post('/api/upload/ubuildit', fd, {
-                transformRequest: angular.identity,
-                headers: { 'Content-Type': undefined }
-            })
+            UploadService.uploadUbuildit(form)
             .then(function(response) {
                 $('#new_project_modal').modal('hide');
-                getProjects();
+                addProject();
             }, function(error) {
                 $scope.new_project_form.$invalid = true;
             });
         } else {
-            $http.post('/api/projects', {
-                name:         $scope.project.name,
-                address:      $scope.project.address,
-                city:         $scope.project.city,
-                state:        $scope.project.state,
-                zipcode:      $scope.project.zipcode,
-                home_sq:      $scope.project.home_sq,
-                project_type: $scope.project.type,
-                user_id:      store.get('user').id
-            })
-            .then(function(response) {
-                $('#new_project_modal').modal('hide');
-                getProjects();
-            }, function(error) {
-                $scope.new_project_form.$invalid = true;
-            });
+            addProject();
         }
+    }
+    function addProject() {
+        ProjectService.addProject($scope.project)
+        .then(function(response) {
+            $('#new_project_modal').modal('hide');
+            getProjects();
+        }, function(error) {
+            $scope.new_project_form.$invalid = true;
+        });
     }
     // DELETE functions
     $scope.showDeleteProjectModal = function() {
-        $('#delete_project_modal').find('.modal-title').text('Delete Project: ' + store.get('project').name);
-        $('#delete_project_modal').find('.modal-title').addClass('text-danger');
-        $('#delete_project_modal').find('.modal-title').css('font-weight', 'Bold');
         $scope.error_msg_delete = false;
         $('#delete_project_modal').modal('show');
     }
     $scope.deleteProject = function() {
-        $http.delete('/api/projects/' + store.get('project').id)
+        ProjectService.deleteProject()
         .then(function(response) {
             $('#delete_project_modal').modal('hide');
             getProjects();
         }, function(error) {
-            $scope.error_msg_delete = 'Could not delete your project.';
+            $scope.error_msg_delete = true;
         });
     }
     // UPDATE functions
     $scope.showEditProjectModal = function(project) {
-        $scope.updated_project         = {};
-        $scope.updated_project.name    = store.get('project').name;
-        $scope.updated_project.address = store.get('project').address;
-        $scope.updated_project.city    = store.get('project').city;
-        $scope.updated_project.state   = store.get('project').state;
-        $scope.updated_project.zipcode = store.get('project').zipcode;
-        $scope.updated_project.home_sq = store.get('project').home_sq;
+        $scope.updated_project = store.get('project');
         $scope.edit_project_form.$setPristine();
-        $('#edit_project_modal').find('.modal-title').text('Edit Project: ' + store.get('project').name);
-        $('#edit_project_modal').find('.modal-title').css('font-weight', 'Bold');
         $('#edit_project_modal').modal('show');
     }
     $scope.updateProject = function() {
-        $http.put('/api/projects/' + store.get('project').id, {
-            name:    $scope.updated_project.name,
-            address: $scope.updated_project.address,
-            city:    $scope.updated_project.city,
-            state:   $scope.updated_project.state,
-            zipcode: $scope.updated_project.zipcode,
-            home_sq: $scope.project.home_sq,
-            user_id: store.get('user').id
-        })
+        ProjectService.updateProject($scope.updated_project)
         .then(function(response) {
             $('#edit_project_modal').modal('hide');
             getProjects();
