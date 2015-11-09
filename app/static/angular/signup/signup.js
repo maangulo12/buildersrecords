42b43732da9e48d8ffc8f058a3321e518f9a2515
Miguel Angulo <maangulo12@gmail.com>
2015-11-09 15:55:47 -0600

    Update signup controller

--- app/static/angular/signup/signup.js
+++ app/static/angular/signup/signup.js
@@ -2,45 +2,34 @@ angular.module('app.signup', [])
 
 .config(function($stateProvider) {
     $stateProvider.state('signup', {
-        url: '/signup',
+        url:         '/signup',
         templateUrl: 'angular/signup/signup.html',
-        controller: 'SignupController'
+        controller:  'SignupController'
     });
 })
-.controller('SignupController', function($scope, store, $state, $http, authService) {
-    store.remove('jwt');
+.controller('SignupController', function($scope, store, $state, UserService, MailService, AuthService) {
+    init();
 
+    function init() {
+        store.remove('jwt');
+    }
     $scope.createAccount = function() {
-        $http.post('/api/users', {
-            username  : $scope.signup.username,
-            password  : $scope.signup.password,
-            first_name: $scope.signup.first_name,
-            last_name : $scope.signup.last_name,
-            email     : $scope.signup.email
-        }).then(function(response) {
-            sendEmail();
-            authenticate();
+        // Add User
+        UserService.addUser($scope.signup)
+        .then(function(response) {
+            // Send Registration Email to User
+            MailService.sendRegistrationEmail($scope.signup);
+            // Authenticate User
+            AuthService.authenticate($scope.signup.username, $scope.signup.password)
+            .then(function(response) {
+                // Store Token
+                AuthService.storeToken(response);
+                $state.go('projects');
+            }, function(error) {
+                $state.go('login');
+            });
         }, function(error) {
             $scope.signup_form.$invalid = true;
-        });
-    }
-    function sendEmail() {
-        $http.post('/api/email/registration', {
-            email     : $scope.signup.email,
-            first_name: $scope.signup.first_name,
-            last_name : $scope.signup.last_name,
-            username  : $scope.signup.username
-        });
-    }
-    function authenticate() {
-        $http.post('/api/auth', {
-            login   : $scope.signup.username,
-            password: $scope.signup.password
-        }).then(function(response) {
-            authService.authHelper(response);
-            $state.go('projects');
-        }, function(error) {
-            $state.go('login');
         });
     }
 });
