f64dcc7c854b91d7dbf3ec7ba82a2139888c8d54
Miguel Angulo <maangulo12@gmail.com>
2015-11-09 15:56:12 -0600

    Update overview module

--- app/static/angular/overview/overview.js
+++ app/static/angular/overview/overview.js
@@ -2,15 +2,15 @@ angular.module('app.overview', [])
 
 .config(function($stateProvider) {
     $stateProvider.state('overview', {
-        url: '/overview',
+        url:         '/overview',
         templateUrl: 'angular/overview/overview.html',
-        controller: 'OverviewController',
-        data: {
+        controller:  'OverviewController',
+        data:        {
             requiresLogin: true
         }
     });
 })
-.controller('OverviewController', function($scope, store, $state, $http) {
+.controller('OverviewController', function($scope, store, CategoryService) {
     init();
 
     function init() {
@@ -18,63 +18,48 @@ angular.module('app.overview', [])
         $scope.project_name = store.get('project').name;
         getCategories();
     }
+    // GET function
     function getCategories() {
-        $http.get('/api/categories?q={"filters":[{"name":"project_id","op":"equals","val":"' + store.get('project').id + '"}]}')
+        CategoryService.getCategories()
         .then(function(response) {
             $scope.category_list = response.data.objects;
 
-            var grand_total_budget = 0;
-            var grand_total_actual = 0;
+            var grand_total_estimated   = 0;
+            var grand_total_actual      = 0;
             var grand_total_expenditure = 0;
 
             angular.forEach(response.data.objects, function(category) {
-                var total_actual = 0;
-                var total_budget = 0;
-                angular.forEach(category.items, function(item) {
-                    total_budget += item.budget;
-                    total_actual += item.actual;
-                });
-                category.total_budget = total_budget;
-                category.total_actual = total_actual;
-                grand_total_budget += total_budget;
-                grand_total_actual += total_actual;
-
+                var total_estimated   = 0;
+                var total_actual      = 0;
                 var total_expenditure = 0;
+                angular.forEach(category.items, function(item) {
+                    total_estimated += item.estimated;
+                    total_actual    += item.actual;
+                });
                 angular.forEach(category.expenditures, function(expenditure) {
                     total_expenditure += expenditure.cost;
                 });
-                category.total_expenditure = total_expenditure;
-                grand_total_expenditure += total_expenditure;
+                category.total_estimated    = total_estimated;
+                category.total_actual       = total_actual;
+                category.total_expenditure  = total_expenditure;
+                grand_total_estimated      += total_estimated;
+                grand_total_actual         += total_actual;
+                grand_total_expenditure    += total_expenditure;
 
                 if (total_actual === 0 || total_expenditure >= total_actual) {
-                    category.paid = 100;
+                    category.paid   = 100;
                     category.unpaid = 0;
                 } else {
-                    category.paid = Math.round(total_expenditure / total_actual * 100);
+                    category.paid   = Math.round(total_expenditure / total_actual * 100);
                     category.unpaid = total_actual - total_expenditure;
                 }
             });
-            $scope.grand_total_budget = grand_total_budget;
-            $scope.grand_total_actual = grand_total_actual;
+            $scope.grand_total_estimated   = grand_total_estimated;
+            $scope.grand_total_actual      = grand_total_actual;
             $scope.grand_total_expenditure = grand_total_expenditure;
 
         }, function(error) {
-            $scope.error_msg = 'Could not load your cost review table.';
+            $scope.error_msg_get = true;
         });
-    }
-    $scope.logOut = function() {
-        store.remove('jwt');
-        $state.go('login');
-    }
-    // UTILITY functions
-    Number.prototype.formatMoney = function(c, d, t) {
-        var n = this,
-            c = isNaN(c = Math.abs(c)) ? 2 : c,
-            d = d == undefined ? "." : d,
-            t = t == undefined ? "," : t,
-            s = n < 0 ? "-" : "",
-            i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
-            j = (j = i.length) > 3 ? j % 3 : 0;
-        return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
     }
 });
