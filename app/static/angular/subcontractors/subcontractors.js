5bcc999ebac6ccc79c8f9719c50dd874794c51c3
Miguel Angulo <maangulo12@gmail.com>
2015-11-10 01:02:26 -0600

    Update subcontractors module

--- app/static/angular/subcontractors/subcontractors.js
+++ app/static/angular/subcontractors/subcontractors.js
@@ -1,32 +1,28 @@
-angular.module('app.subcontractors', [])
+var app = angular.module('app.subcontractors', []);
 
-.config(function($stateProvider) {
+app.config(function($stateProvider) {
     $stateProvider.state('subcontractors', {
-        url: '/subcontractors',
+        url:         '/subcontractors',
         templateUrl: 'angular/subcontractors/subcontractors.html',
-        controller: 'SubcontractorController',
-        data: {
+        controller:  'SubcontractorController',
+        data:        {
             requiresLogin: true
         }
     });
-})
-.controller('SubcontractorController', function($scope, store, $state, $http) {
+});
+
+app.controller('SubcontractorController', function($scope, store, SubcontractorService) {
     init();
 
     function init() {
         $scope.username = store.get('user').username;
         getSubcontractors();
     }
-    $scope.logOut = function() {
-        store.remove('jwt');
-        $state.go('login');
-    }
     function getSubcontractors() {
-        $http.get('/api/subcontractors?q={"filters":[{"name":"project_id","op":"equals","val":"' + store.get('project').id + '"}]}')
-        .then(function(response) {
+        SubcontractorService.getSubcontractors().then(function(response) {
             $scope.subcontractor_list = response.data.objects;
         }, function(error) {
-            $scope.error_msg = 'Could not load your subcontractor list. Please try to refresh the page.';
+            $scope.error_msg_get = true;
         });
     }
 });
