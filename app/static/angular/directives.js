11cdb92de6d5634a4b38e5dd2798a2ffa2f164b8
Miguel Angulo <maangulo12@gmail.com>
2015-11-09 23:55:44 -0600

    Update directives module

--- app/static/angular/directives.js
+++ app/static/angular/directives.js
@@ -1,62 +1,55 @@
-// This module contains all of the directives of this Angular app.
-// Directives are markers on a DOM element that tell Angular compiler
-// to attach a specified behavior to that DOM element.
-angular.module('app.directives', [])
+// Directives of this Angular app
+var app = angular.module('app.directives', []);
 
-.directive('usernameAvailability', function($http, $q) {
-    // This directive checks if an username is available.
-    // It basically goes to the backend and checks if that username already exists.
+// DIRECTIVE: username-availability
+// Checks if an username is available
+app.directive('usernameAvailability', function($q, UserService) {
     return {
         require: 'ngModel',
         link: function(scope, element, attrs, ctrl) {
             ctrl.$asyncValidators.usernameAvailability = function(username) {
-
-                var promise = $http.get('/api/users?q={"filters":[{"name":"username","op":"equals","val":"' + username + '"}]}');
-                var success = function(response) {
+                return UserService.getUserbyUsername(username).then(function(response) {
                     if (response.data.num_results == 0) {
                         ctrl.$setValidity('usernameAvailability', true);
                     } else {
                         ctrl.$setValidity('usernameAvailability', false);
                         return $q.reject(response);
                     }
-                }
-                var failure = function(response) {
+                }, function(response) {
                     ctrl.$setValidity('usernameAvailability', false);
                     return $q.reject(response);
-                }
-                return promise.then(success, failure);
+                });
             }
         }
     }
-})
-.directive('emailAvailability', function($http, $q) {
-    // This directive checks if an email is available.
-    // It goes to the backend and checks if that email already exists.
+});
+
+// DIRECTIVE: email-availability
+// Checks if an email already exists
+app.directive('emailAvailability', function($q, UserService) {
     return {
         require: 'ngModel',
         link: function(scope, element, attrs, ctrl) {
             ctrl.$asyncValidators.emailAvailability = function(email) {
-
-                var promise = $http.get('/api/users?q={"filters":[{"name":"email","op":"equals","val":"' + email + '"}]}');
-                var success = function(response) {
+                return UserService.getUserbyEmail(email).then(function(response) {
                     if (response.data.num_results == 0) {
                         ctrl.$setValidity('emailAvailability', true);
                     } else {
                         ctrl.$setValidity('emailAvailability', false);
                         return $q.reject(response);
                     }
-                }
-                var failure = function(response) {
+                }, function(response) {
                     ctrl.$setValidity('emailAvailability', false);
                     return $q.reject(response);
-                }
-                return promise.then(success, failure);
+                });
             }
         }
     }
-})
-.directive('format', function ($filter) {
-    // This directive is used for formatting input field to allow only numbers
+});
+
+// DIRECTIVE: format='number'
+// Formats input field to allow numbers only
+app.directive('format', function ($filter) {
     return {
         require: 'ngModel',
         link: function (scope, element, attrs, ctrl) {
@@ -73,9 +66,11 @@ angular.module('app.directives', [])
             });
         }
     }
-})
-.directive('pageSelect', function() {
-    // This directive is used smart-table custom pagination
+});
+
+// DIRECTIVE: page-select
+// Smart-table custom pagination
+app.directive('pageSelect', function() {
     return {
         restrict: 'E',
         template: '<input type="text" class="select-page" ng-model="inputPage" ng-change="selectPage(inputPage)">',
