a3390db5e67cc323caed691c45853d8fcf9b61d6
Miguel Angulo <maangulo12@gmail.com>
2015-11-09 23:56:34 -0600

    Update login controller

--- app/static/angular/login/login.js
+++ app/static/angular/login/login.js
@@ -1,23 +1,22 @@
-angular.module('app.login', [])
+var app = angular.module('app.login', []);
 
-.config(function($stateProvider) {
+app.config(function($stateProvider) {
     $stateProvider.state('login', {
         url:         '/login',
         templateUrl: 'angular/login/login.html',
         controller:  'LoginController'
     });
-})
-.controller('LoginController', function($scope, store, $state, AuthService) {
+});
+
+app.controller('LoginController', function($scope, store, $state, AuthService) {
     init();
 
     function init() {
         store.remove('jwt');
     }
     $scope.logIn = function() {
-        // Authenticate User
         AuthService.authenticate($scope.login.user, $scope.login.password)
         .then(function(response) {
-            // Store Token
             AuthService.storeToken(response);
             $state.go('projects');
         }, function(error) {
