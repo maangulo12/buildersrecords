65c5af1b1010b392048f43fc36ab11abe3ac9d60
Miguel Angulo <maangulo12@gmail.com>
2015-11-09 23:56:46 -0600

    Update signup controller

--- app/static/angular/signup/signup.js
+++ app/static/angular/signup/signup.js
@@ -1,28 +1,24 @@
-angular.module('app.signup', [])
+var app = angular.module('app.signup', []);
 
-.config(function($stateProvider) {
+app.config(function($stateProvider) {
     $stateProvider.state('signup', {
         url:         '/signup',
         templateUrl: 'angular/signup/signup.html',
         controller:  'SignupController'
     });
-})
-.controller('SignupController', function($scope, store, $state, UserService, MailService, AuthService) {
+});
+
+app.controller('SignupController', function($scope, store, $state, UserService, MailService, AuthService) {
     init();
 
     function init() {
         store.remove('jwt');
     }
     $scope.createAccount = function() {
-        // Add User
-        UserService.addUser($scope.signup)
-        .then(function(response) {
-            // Send Registration Email to User
+        UserService.addUser($scope.signup).then(function(response) {
             MailService.sendRegistrationEmail($scope.signup);
-            // Authenticate User
             AuthService.authenticate($scope.signup.username, $scope.signup.password)
             .then(function(response) {
-                // Store Token
                 AuthService.storeToken(response);
                 $state.go('projects');
             }, function(error) {
