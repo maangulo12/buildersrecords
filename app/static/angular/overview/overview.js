415cf8ea8a72022c597444beba194371aaf594e6
Miguel Angulo <maangulo12@gmail.com>
2015-11-11 21:49:04 -0600

    Update overview module

--- app/static/angular/overview/overview.js
+++ app/static/angular/overview/overview.js
@@ -17,7 +17,45 @@ app.controller('OverviewController', function($scope, store, CategoryService) {
     function init() {
         $scope.username = store.get('user').username;
         $scope.project_name = store.get('project').name;
+        drawBarChart();
         getCategories();
+    }
+    function drawBarChart() {
+        $('#barchart-container').highcharts({
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: '',
+                style: {
+                    display: 'none'
+                }
+            },
+            subtitle: {
+                text: '',
+                style: {
+                    display: 'none'
+                }
+            },
+            xAxis: {
+                categories: ['Apples', 'Bananas', 'Oranges']
+            },
+            yAxis: {
+                title: {
+                    text: 'Fruit eaten'
+                }
+            },
+            series: [{
+                name: 'Jane',
+                data: [1, 0, 4]
+            }, {
+                name: 'John',
+                data: [5, 7, 3]
+            }],
+            credits: {
+                enabled: false
+            }
+        });
     }
     // GET function
     function getCategories() {
