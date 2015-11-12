7d5aa459a40e264b8038122a646f4baa15b8678c
Miguel Angulo <maangulo12@gmail.com>
2015-11-12 02:21:40 -0600

    Update budget module

--- app/static/angular/budget/budget.js
+++ app/static/angular/budget/budget.js
@@ -12,13 +12,80 @@ app.config(function($stateProvider) {
 });
 
 app.controller('BudgetController', function($scope, store, CategoryService, ItemService) {
+    var options = [];
+    var piechart = null;
     init();
 
     function init() {
         $scope.username = store.get('user').username;
-        drawPieChart();
-        getCategories();
+        initPiechart();
+        updatePiechart();
+        updateTable();
     }
+
+    // PIECHART functions
+    function initPiechart() {
+        options = {
+            chart: {
+                type: 'pie',
+                style: {
+                    fontFamily: "Montserrat, 'Helvetica Neue', Helvetica, Arial, sans-serif"
+                }
+            },
+            title: {
+                text: ''
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size: 14px"> {point.key} </span><br>',
+                pointFormat:  "<span style=\"font-size: 14px\"> <b> ${point.y:.2f} </b> </span><br>"
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<span style="font-size: 14px"> {point.name} </span><br> {point.percentage:.2f}%'
+                    }
+                }
+            },
+            series: [{
+                name: 'Categories',
+                data: []
+            }],
+            credits: {
+                enabled: false
+            }
+        };
+
+        piechart = $('#piechart-container').highcharts(options);
+    }
+    function updatePiechart() {
+        CategoryService.getCategories().then(function(response) {
+            options.series[0].data = [];
+
+            if (response.data.objects.length == 0) {
+                options.series[0].data.push({ name: 'No Categories', y: 0.01 });
+            } else {
+                angular.forEach(response.data.objects, function(category) {
+                    if (category.items.length != 0) {
+                        var category_total = 0;
+
+                        angular.forEach(category.items, function(item) {
+                            category_total += item.actual;
+                        });
+                        options.series[0].data.push({ name: category.name, y: category_total });
+                    }
+                });
+            }
+            piechart = new Highcharts.Chart(options);
+
+        }, function(error) {
+            $scope.error_msg_get = true;
+        });
+    }
+
+    // CLICKED EVENTS
     $scope.clickedItem = function(item) {
         var index = $scope.category.items.indexOf(item);
         if (index !== -1) {
@@ -40,61 +107,9 @@ app.controller('BudgetController', function($scope, store, CategoryService, Item
             $scope.selected = is_selected;
         }
     }
+
     // GET function
-    function drawPieChart() {
-        CategoryService.getCategories()
-        .then(function(response) {
-            var i              = 0;
-            var data           = [];
-            var colors         = [];
-            var start_color    = 'mediumspringgreen';
-            var end_color      = 'darkslategray';
-            var gray_color     = '#AAAAAA';
-            var num_categories = response.data.objects.length;
-
-            if (num_categories == 0) {
-                data.push({
-                    value:     0.01,
-                    color:     gray_color,
-                    highlight: gray_color,
-                    label:     'No categories'
-                });
-            } else if (num_categories == 1) {
-                colors.push(start_color);
-            } else {
-                colors = getColorList(num_categories, start_color, end_color);
-            }
-
-            angular.forEach(response.data.objects, function(category) {
-                var total_actual = 0;
-
-                angular.forEach(category.items, function(item) {
-                    total_actual += item.actual;
-                });
-                data.push({
-                    value:     total_actual,
-                    color:     colors[i],
-                    highlight: colors[i],
-                    label:     category.name
-                });
-                i++;
-            });
-            var ctx = $('#modular-doughnut').get(0).getContext('2d');
-            var chart = new Chart(ctx).Doughnut(data, {
-                scaleIntegersOnly: false,
-                responsive:        true,
-                tooltipTemplate:   "<%if (label){%><%=label%>: <%}%>$<%= value.formatMoney(2, '.', ',') %>",
-                legendTemplate:    "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><div class=\"comm-how\">$<%=segments[i].value.formatMoney(2, '.', ',')%></div><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
-            });
-            var legend       = document.createElement('div');
-            legend.innerHTML = chart.generateLegend();
-            document.getElementById('legend-holder').appendChild(legend.firstChild);
-
-        }, function(error) {
-            $scope.error_msg_get = true;
-        });
-    }
-    function getCategories() {
+    function updateTable() {
         CategoryService.getCategories()
         .then(function(response) {
             $scope.category_list = response.data.objects;
@@ -122,6 +137,7 @@ app.controller('BudgetController', function($scope, store, CategoryService, Item
             $scope.error_msg_get = true;
         });
     }
+
     // ADD functions
     $scope.showAddItemModal = function() {
         $scope.item = {};
@@ -130,8 +146,7 @@ app.controller('BudgetController', function($scope, store, CategoryService, Item
     }
     $scope.addItem = function() {
         if ($scope.item.new_category) {
-            CategoryService.addCategory($scope.item.new_category)
-            .then(function(response) {
+            CategoryService.addCategory($scope.item.new_category).then(function(response) {
                 $scope.item.category = response.data.id
                 addItem($scope.item);
             }, function(error) {
@@ -145,12 +160,14 @@ app.controller('BudgetController', function($scope, store, CategoryService, Item
         ItemService.addItem($scope.item)
         .then(function(response) {
             $('#add_item_modal').modal('hide');
-            getCategories();
+            updatePiechart()
+            updateTable();
         }, function(error) {
             $scope.add_item_form.$invalid = true;
         });
     }
-    // DELETE EXPENDITURES functions
+
+    // DELETE ITEM functions
     $scope.showDeleteItemsModal = function() {
         if (!$('#delete_button').hasClass('disabled')) {
             $('#delete_items_modal').modal('show');
@@ -160,10 +177,8 @@ app.controller('BudgetController', function($scope, store, CategoryService, Item
         angular.forEach($scope.category_list, function(category) {
             angular.forEach(category.items, function(item) {
                 if (item.selected) {
-                    ItemService.deleteItem(item.id)
-                    .then(function(response) {
+                    ItemService.deleteItem(item.id).then(function(response) {
                         $('#delete_items_modal').modal('hide');
-                        getCategories();
                         $scope.selected = false;
                     }, function(error) {
                         $scope.error_msg_delete = true;
@@ -171,39 +186,21 @@ app.controller('BudgetController', function($scope, store, CategoryService, Item
                 }
             });
         });
+        updatePiechart();
+        updateTable();
     }
+
+    // DELETE SINGLE ITEM functions
     $scope.showSingleDeleteItemModal = function() {
         $('#delete_single_item_modal').modal('show');
     }
     $scope.deleteSingleItem = function() {
-        ItemService.deleteItem(store.get('item').id)
-        .then(function(response) {
+        ItemService.deleteItem(store.get('item').id).then(function(response) {
             $('#delete_single_item_modal').modal('hide');
-            getCategories();
+            updatePiechart()
+            updateTable();
         }, function(error) {
             $scope.error_msg_delete_single = true;
         });
-    }
-    // UTILITY functions
-    function getColorList(num_items, start_spec, end_spec) {
-        var colors = [];
-        var rainbow = new Rainbow();
-        rainbow.setNumberRange(1, num_items);
-        rainbow.setSpectrum(start_spec, end_spec);
-
-        for (var i = 1; i <= num_items; i++) {
-            colors.push('#' + rainbow.colourAt(i));
-        }
-        return colors;
-    }
-    Number.prototype.formatMoney = function(c, d, t) {
-        var n = this,
-            c = isNaN(c = Math.abs(c)) ? 2 : c,
-            d = d == undefined ? "." : d,
-            t = t == undefined ? "," : t,
-            s = n < 0 ? "-" : "",
-            i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
-            j = (j = i.length) > 3 ? j % 3 : 0;
-        return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
     }
 });
