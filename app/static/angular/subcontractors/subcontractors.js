d4389ffba94c8f704f7947540ecaf9fb8e15fd3f
Miguel Angulo <maangulo12@gmail.com>
2015-11-13 01:31:19 -0600

    Update subcontractors module

--- app/static/angular/subcontractors/subcontractors.js
+++ app/static/angular/subcontractors/subcontractors.js
@@ -18,11 +18,20 @@ app.controller('SubcontractorController', function($scope, store, SubcontractorS
         $scope.username = store.get('user').username;
         getSubcontractors();
     }
+
+    // GET SUBCONTRACTOR function
     function getSubcontractors() {
         SubcontractorService.getSubcontractors().then(function(response) {
             $scope.subcontractor_list = response.data.objects;
         }, function(error) {
             $scope.error_msg_get = true;
         });
+    }
+
+    $scope.showAddSubcontractorModal = function() {
+
+    }
+    $scope.addSubcontractor = function() {
+
     }
 });
