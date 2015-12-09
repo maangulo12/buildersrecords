06d93c3ccea00af7ff22f824c41f14eb5111b487
Miguel Angulo <maangulo12@gmail.com>
2015-12-09 13:32:25 -0600

    Add subscriptions module

---
+++ app/api_subscriptions.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    app.api_subscriptions
+    ~~~~~~~~~~~
+
+    This module is used for subscriptions.
+"""
+
+import stripe
+from flask import current_app
+
+from app import app
+
+
+API_ENTRY = '/api/subscriptions'
+
+stripe.api_key = current_app.config['STRIPE_API_KEY']
+
+
+# Needs route security
+@app.route(API_ENTRY, methods=['POST'])
+def subscriptions():
+    """
+    Creates a customer and a subscription for the customer.
+
+    POST: {
+
+    }
+    """
+    # Get JSON data
+
+    # Create Customer
+    stripe.Customer.create(
+        email=""
+        description="Customer for test@example.com",
+        source="tok_17EE2cD49oGyKMoCirkykocN" # obtained with Stripe.js
+    )
+
+    # Subscribe Customer
