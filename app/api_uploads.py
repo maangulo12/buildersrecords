ff6d83d459328afb7acfedf798e5da54510cb2d8
Miguel Angulo <maangulo12@gmail.com>
2015-12-10 02:39:43 -0600

    Update api_uploads module

--- app/api_uploads.py
+++ app/api_uploads.py
@@ -11,10 +11,7 @@
         -ubuildit : /api/upload/ubuildit (POST)
 """
 
-from uuid import uuid4
-from flask import make_response, request, current_app
-from boto.s3.connection import S3Connection
-from boto.s3.key import Key
+from flask import make_response, request
 
 from app import app, db
 from app.models import Project, Category, Item
@@ -22,34 +19,6 @@ from app.utility import parse_ubuildit_file
 
 
 API_ENTRY = '/api/uploads'
-
-
-def upload_file(key, file_obj):
-    """
-    Uploads a file to AWS S3.
-        :param key: AWS bucket key
-        :param file_obj: the file as a FileStorage object
-        :return: True if the file is uploaded
-    """
-    try:
-        data = file_obj.read()
-        key.set_contents_from_string(data)
-        return True
-
-    except:
-        return False
-
-
-def get_bucket_key(path):
-    """
-    Gets the AWS Bucket Key.
-        :param path: the path to store the file
-        :return: AWS bucket key
-    """
-    conn   = S3Connection(current_app.config['AWS_ACCESS_KEY'], current_app.config['AWS_SECRET_KEY'])
-    bucket = conn.get_bucket(current_app.config['S3_BUCKET'])
-    key    = Key(bucket=bucket, name=path)
-    return key
 
 
 # Needs route security
@@ -87,42 +56,33 @@ def ubuildit():
     if not all(criterion):
         return make_response('Bad request', 400)
 
-    path = current_app.config['UPLOAD_PATH'] + '/' + user_id + '/' + name + '/' + str(uuid4())
-    key  = get_bucket_key(path)
-    data = file_obj.read()
+    try:
+        data = file_obj.read()
+        category_list = parse_ubuildit_file(data)
 
-    if key is None:
-        return make_response('Could not get AWS bucket key', 400)
+        project = Project(name=name, address=address, city=city,
+                          state=state, zipcode=zipcode, home_sq=home_sq,
+                          project_type=project_type, user_id=user_id)
+        db.session.add(project)
+        db.session.commit()
 
-    if upload_file(key, file_obj):
-        try:
-            category_list = parse_ubuildit_file(data)
-
-            project = Project(name=name, address=address, city=city,
-                              state=state, zipcode=zipcode, home_sq=home_sq,
-                              project_type=project_type, user_id=user_id)
-            db.session.add(project)
+        for cat in category_list:
+            category = Category(name=cat['category_name'],
+                                project_id=project.id)
+            db.session.add(category)
             db.session.commit()
 
-            for cat in category_list:
-                category = Category(name=cat['category_name'],
-                                    project_id=project.id)
-                db.session.add(category)
+            for cat_item in cat['item_list']:
+                item = Item(name=cat_item['cost_category'],
+                            description=cat_item['description'],
+                            estimated=cat_item['estimated'],
+                            actual=cat_item['actual'],
+                            category_id=category.id,
+                            project_id=project.id)
+                db.session.add(item)
                 db.session.commit()
 
-                for cat_item in cat['item_list']:
-                    item = Item(name=cat_item['cost_category'],
-                                description=cat_item['description'],
-                                estimated=cat_item['estimated'],
-                                actual=cat_item['actual'],
-                                category_id=category.id,
-                                project_id=project.id)
-                    db.session.add(item)
-                    db.session.commit()
+        return make_response('Success! The file was uploaded', 201)
 
-            return make_response('Success! The file was uploaded', 201)
-
-        except:
-            return make_response('The file could not be read', 400)
-    else:
-        return make_response('Could not upload file', 400)
+    except:
+        return make_response('The file could not be read', 400)
