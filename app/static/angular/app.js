9ccc5c833832178280be7bcbde9b9b596958ff3d
Miguel A. Angulo <maangulo12@gmail.com>
2015-12-14 17:20:02 -0600

    Remove monthly

--- app/static/angular/app.js
+++ app/static/angular/app.js
@@ -11,7 +11,6 @@ var app = angular.module('app', [
     'app.home',
     'app.login',
     'app.signup',
-    'app.signup.monthly',
     'app.projects',
     'app.projects.overview',
     'app.projects.budget',
