3709c7a6d7fc3d6b1e4fd7ad787cb2b444ff8f90
Miguel Angulo <maangulo12@gmail.com>
2015-12-15 14:39:30 -0600

    Update signup module

--- app/static/angular/modules/signup/signup.js
+++ app/static/angular/modules/signup/signup.js
@@ -20,6 +20,9 @@ app.controller('SignupController', function($scope, store, $state, UserService, 
 
     $scope.createAccount = function() {
         var btn = $('#create-account-btn').button('loading');
+
+        // call SubscriptionService
+
         UserService.addUser($scope.signup).then(function(response) {
             MailService.sendRegistrationEmail($scope.signup);
             AuthService.authenticate($scope.signup.username, $scope.signup.password)
