95f90fa7b97268ca490fe1183515ab20be44dad8
Miguel Angulo <maangulo12@gmail.com>
2015-12-19 18:29:23 -0600

    Update account module

--- app/static/angular/modules/account/account.js
+++ app/static/angular/modules/account/account.js
@@ -21,7 +21,7 @@ app.config(function($stateProvider) {
     });
 });
 
-app.controller('AccountController', function($scope, store, UserService) {
+app.controller('AccountController', function($scope, store, SubscriptionService) {
     init();
 
     function init() {
@@ -29,5 +29,17 @@ app.controller('AccountController', function($scope, store, UserService) {
         $scope.user.date_created = store.get('user').date_created;
         $scope.user.email        = store.get('user').email;
         $scope.user.username     = store.get('user').username;
+
+        SubscriptionService.getSubscription().then(function(response) {
+            $scope.user.card = {};
+            $scope.user.card.name = response.data.sources.data[0].name;
+            $scope.user.card.last4 = response.data.sources.data[0].last4;
+            $scope.user.card.brand = response.data.sources.data[0].brand;
+            $scope.user.sub_plan = response.data.subscriptions.data[0].plan.id;
+        }, function(error) {
+            $scope.error_msg = true;
+        });
     }
+
+    // Changing email should update Stripe
 });
