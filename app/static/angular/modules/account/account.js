fa79ea45dc9d0a8b023c7d919e4207942767c72b
Miguel Angulo <maangulo12@gmail.com>
2015-12-20 21:50:39 -0600

    Update account module

--- app/static/angular/modules/account/account.js
+++ app/static/angular/modules/account/account.js
@@ -31,15 +31,20 @@ app.controller('AccountController', function($scope, store, SubscriptionService)
         $scope.user.username     = store.get('user').username;
 
         SubscriptionService.getSubscription().then(function(response) {
-            $scope.user.card = {};
-            $scope.user.card.name = response.data.sources.data[0].name;
+            $scope.user.card       = {};
+            $scope.user.card.name  = response.data.sources.data[0].name;
             $scope.user.card.last4 = response.data.sources.data[0].last4;
             $scope.user.card.brand = response.data.sources.data[0].brand;
-            $scope.user.sub_plan = response.data.subscriptions.data[0].plan.id;
+            $scope.user.sub_plan   = response.data.subscriptions.data[0].plan.id;
         }, function(error) {
             $scope.error_msg = true;
         });
     }
-
     // Changing email should update Stripe
+
+    $scope.updatePassword = function() {
+        var btn = $('#update-password-btn').button('loading');
+
+        // Needs work     
+    }
 });
