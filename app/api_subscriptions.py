47f45340a4388049d77c6a6f3ea1302e9cb90c98
Miguel Angulo <maangulo12@gmail.com>
2015-12-20 21:49:57 -0600

    Handle stripe errors successfully

--- app/api_subscriptions.py
+++ app/api_subscriptions.py
@@ -9,7 +9,7 @@
 """
 
 import stripe
-from flask import current_app, request, make_response, jsonify
+from flask import request, make_response, jsonify
 
 from app import app, db
 from app.models import User
@@ -27,7 +27,7 @@ def subscribe_customer():
         email    : 'email address'
         username : 'username'
         password : 'password'
-        sub_plan : 'subscription plan'
+        plan     : 'subscription plan'
         token_id : 'stripe token id'
     }
     """
@@ -35,45 +35,75 @@ def subscribe_customer():
     email     = data.get('email', None)
     username  = data.get('username', None)
     password  = data.get('password', None)
-    sub_plan  = data.get('sub_plan', None)
+    plan      = data.get('plan', None)
     token_id  = data.get('token_id', None)
-    criterion = [email, username, password, sub_plan, token_id, len(data) == 5]
+    criterion = [email,
+                username,
+                password,
+                plan,
+                token_id,
+                len(data) == 5,
+                len(email) <= 50,
+                4 <= len(username) <= 30]
 
     if not all(criterion):
-        return make_response('Bad Request', 400)
+        print('ERROR 404: Bad request')
+        return make_response('Bad request', 400)
 
-    customer = stripe.Customer.create(
-        email=email,
-        plan=sub_plan,
-        source=token_id
-    )
+    try:
+        customer = stripe.Customer.create(
+            email=email,
+            plan=plan,
+            source=token_id
+        )
+        user = User(
+            email=email,
+            username=username,
+            password=password,
+            stripe_id=customer['id']
+        )
+        db.session.add(user)
+        db.session.commit()
+        return make_response('Customer succesfully subscribed', 201)
 
-    if customer is None:
-        return make_response('Customer could not be subscribed', 400)
+    except (stripe.error.CardError, e):
+        print('ERROR 404: Card declined')
+        return make_response('Card declined', 400)
 
-    user = User(
-        email=email,
-        username=username,
-        password=password,
-        stripe_id=customer['id']
-    )
-    db.session.add(user)
-    db.session.commit()
+    except (stripe.error.RateLimitError, e):
+        print('ERROR 404: Too many requests made to Stripe')
+        return make_response('Too many requests made to Stripe', 400)
 
-    return make_response('Customer succesfully subscribed', 201)
+    except (stripe.error.InvalidRequestError, e):
+        print('ERROR 404: Invalid parameters were supplied to Stripe')
+        return make_response('Invalid parameters were supplied to Stripe', 400)
+
+    except (stripe.error.AuthenticationError, e):
+        print('ERROR 404: Authentication with Stripe failed')
+        return make_response('Authentication with Stripe failed', 400)
+
+    except (stripe.error.APIConnectionError, e):
+        print('ERROR 404: Network communication with Stripe failed')
+        return make_response('Network communication with Stripe failed', 400)
+
+    except (stripe.error.StripeError, e):
+        print('ERROR 404: Stripe Error')
+        return make_response('Stripe Error', 400)
+
+    except (Exception, e):
+        print('ERROR 404: Error')
+        return make_response('Error', 400)
 
 
 # Needs route security
 @app.route(API_ENTRY + '/<stripe_id>', methods=['GET'])
 def get_customer(stripe_id):
     """
-    Get customer data from stripe_id.
+    Get customer data from Stripe.
     """
     customer = stripe.Customer.retrieve(stripe_id)
 
     if customer is None:
         return make_response('Could not retrieve customer', 400)
-
-    print(customer)
 
     return make_response(jsonify(customer), 200)
