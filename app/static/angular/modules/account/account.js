b84cc316f32fa81532c7cc2d5676bfe983de6c0e
Miguel Angulo <maangulo12@gmail.com>
2015-12-23 15:13:42 -0600

    Update account module

--- app/static/angular/modules/account/account.js
+++ app/static/angular/modules/account/account.js
@@ -21,46 +21,23 @@ app.config(function($stateProvider) {
     });
 });
 
-app.controller('AccountController', function($scope, store, SubscriptionService, UserService) {
+app.controller('AccountController', function($scope, store, UserService) {
     init();
 
     function init() {
-        $scope.user = {};
-        $scope.user.date_created = store.get('user').date_created;
-        $scope.user.email        = store.get('user').email;
-        $scope.user.username     = store.get('user').username;
-
-        SubscriptionService.getSubscription()
-        .then(function(response) {
-            $scope.user.card       = {};
-            $scope.user.card.name  = response.data.sources.data[0].name;
-            $scope.user.card.last4 = response.data.sources.data[0].last4;
-            $scope.user.card.brand = response.data.sources.data[0].brand;
-            $scope.user.sub_plan   = response.data.subscriptions.data[0].plan.id;
-        }, function(error) {
-            $scope.error_msg = true;
-        });
-    }
-
-    // Changing email should update Stripe
-
-    $scope.showUpdateCardModal = function() {
-        var btn = $('#create-project-btn').button('reset');
-        // $scope.project = {};
-        // $scope.new_project_form.$setPristine();
-        $('#update_card').modal('show');
+        $scope.user          = {};
+        $scope.user.email    = store.get('user').email;
+        $scope.user.username = store.get('user').username;
     }
 
     $scope.updatePassword = function() {
+        // Check if current password matches
+        // Hash new password in the backend
         var btn = $('#update-password-btn').button('loading');
         UserService.updatePassword($scope.user.new_password)
         .then(function(response) {
-            alert('Password updated');
-            $scope.update_password_success = true;
             btn.button('reset');
         }, function(error) {
-            alert('Password could not be updated');
-            $scope.update_password_failure = true;
             $scope.password_form.$invalid = true;
             btn.button('reset');
         });
