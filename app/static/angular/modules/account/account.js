922351f0b2365358c214d3293534e668d8b42ec6
Miguel Angulo <maangulo12@gmail.com>
2015-12-24 01:47:19 -0600

    Update account module

--- app/static/angular/modules/account/account.js
+++ app/static/angular/modules/account/account.js
@@ -21,13 +21,32 @@ app.config(function($stateProvider) {
     });
 });
 
-app.controller('AccountController', function($scope, store, UserService) {
+app.controller('AccountController', function($scope, store, UserService, UtilityService) {
     init();
 
     function init() {
         $scope.user          = {};
         $scope.user.email    = store.get('user').email;
         $scope.user.username = store.get('user').username;
+    }
+
+    $scope.updateEmail = function() {
+        $scope.update_email_success = false;
+        $scope.update_email_error = false;
+        var btn = $('#update_email_btn').button('loading');
+        UserService.updateEmail($scope.user.email)
+        .then(function(response) {
+            $scope.update_email_success = true;
+            btn.button('reset');
+        }, function(error) {
+            $scope.email_form.$invalid = true;
+            $scope.update_email_error = true;
+            btn.button('reset');
+        });
+    }
+
+    $scope.updateUsername = function() {
+
     }
 
     $scope.updatePassword = function() {
