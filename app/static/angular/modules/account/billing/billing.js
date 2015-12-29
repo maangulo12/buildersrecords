f0fc6b2548719ee2f1087974f0e89a30bfc41096
Miguel Angulo <maangulo12@gmail.com>
2015-12-29 13:10:32 -0600

    refactor(billing): Separate function handlers

--- app/static/angular/modules/account/billing/billing.js
+++ app/static/angular/modules/account/billing/billing.js
@@ -4,28 +4,21 @@ app.config(function($stateProvider) {
     $stateProvider.state('billing', {
         url: '/account/billing',
         resolve: {
-            User: function(UserService, store) {
-                return UserService.getUser(store.get('user').id)
-                .then(function(response) {
+            UserObj: function(User, $q) {
+                return User.retrieve().then(responseHandler, errorHandler);
+                function responseHandler(response) {
                     return response.data;
-                }, function(error) {
-                    return 'error';
-                });
-            },
-            Subscription: function(SubscriptionService, store) {
-                return SubscriptionService.getSubscription()
-                .then(function(response) {
-                    return response.data;
-                }, function(error) {
-                    return 'error';
-                });
+                }
+                function errorHandler(response) {
+                    return $q.reject(response.data);
+                }
             }
         },
         views: {
             'nav': {
                 templateUrl: 'static/angular/components/navs/nav2.html',
-                controller: function($scope, User) {
-                    $scope.username = User.username;
+                controller: function($scope, UserObj) {
+                    $scope.username = UserObj.username;
                 }
             },
             'body': {
@@ -40,20 +33,65 @@ app.config(function($stateProvider) {
 });
 
 app.controller('BillingController', function($scope, Subscription) {
-    init();
+    update();
 
-    function init() {
-        $scope.user = {};
-        $scope.user.card       = {};
-        $scope.user.card.name  = Subscription.sources.data[0].name;
-        $scope.user.card.last4 = Subscription.sources.data[0].last4;
-        $scope.user.card.brand = Subscription.sources.data[0].brand;
-        $scope.user.plan       = Subscription.subscriptions.data[0].plan.id;
+    function update() {
+        Subscription.retrieve().then(responseHandler, errorHandler);
+        function responseHandler(response) {
+            $scope.user = {};
+            $scope.user.card = {};
+            $scope.user.card.name  = response.data.sources.data[0].name;
+            $scope.user.card.last4 = response.data.sources.data[0].last4;
+            $scope.user.card.brand = response.data.sources.data[0].brand;
+            $scope.user.plan       = response.data.subscriptions.data[0].plan.id;
+        }
+        function errorHandler(response) {
+            $scope.error_msg_get = true;
+        }
     }
 
     $scope.showUpdateCardModal = function() {
-        // $scope.project = {};
-        // $scope.new_project_form.$setPristine();
+        $scope.updated_card = {};
+        $scope.updated_card_form.$setPristine();
         $('#update_card_modal').modal('show');
+    }
+    $scope.updateCard = function() {
+        var btn = $('#update_card_button').button('loading');
+        var val_card = Stripe.card.validateCardNumber($scope.updated_card.card_number);
+        var val_exp  = Stripe.card.validateExpiry($scope.updated_card.exp_month, $scope.updated_card.exp_year);
+        var val_cvc  = Stripe.card.validateExpiry($scope.updated_card.cvc);
+
+        if (val_card && val_exp && val_cvc) {
+            var data = {
+                number:    $scope.updated_card.card_number,
+                cvc:       $scope.updated_card.cvc,
+                exp_month: $scope.updated_card.exp_month,
+                exp_year:  $scope.updated_card.exp_year,
+                name:      $scope.updated_card.card_name
+            };
+            // Remember to change this to live key
+            Stripe.setPublishableKey('pk_test_KY3H8e295UxwoHrrqHBobKRC');
+            Stripe.card.createToken(data, stripeResponseHandler);
+        } else {
+            error();
+        }
+        function stripeResponseHandler(status, response) {
+            if (response.error) {
+                error();
+            } else {
+                Subscription.update(response.id).then(responseHandler, errorHandler);
+            }
+        }
+        function responseHandler(response) {
+            $('#update_card_modal').modal('hide');
+            update();
+        }
+        function errorHandler(response) {
+            error();
+        }
+        function error() {
+            $scope.updated_card_form.$invalid = true;
+            btn.button('reset');
+        }
     }
 });
