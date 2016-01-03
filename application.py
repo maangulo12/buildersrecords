e85ac4324a942866c62111cd5bee0cab67a8958f
Miguel A. Angulo <maangulo12@gmail.com>
2016-01-03 13:23:46 -0600

    refactor(application): run app with a function

--- application.py
+++ application.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 """
     BuildersRecords
     --------------------
-    :copyright: (c) 2015
+    :copyright: (c) 2016
 
     Run this module to deploy the application.
 
@@ -12,9 +11,19 @@
         python3 application.py
 """
 
-from app import app
+from app import create_app
 from app.settings import SERVER_HOST, SERVER_PORT, SERVER_DEBUG
 
 
-if __name__ == '__main__':
+def run_app():
+    """
+        Runs the Flask application.
+    """
+    # Create the application
+    app = create_app()
+    # Run the application
     app.run(host=SERVER_HOST, port=SERVER_PORT, debug=SERVER_DEBUG)
+
+
+if __name__ == '__main__':
+    run_app()
