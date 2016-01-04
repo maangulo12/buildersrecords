ff77680c0cd5b71d8564e8b7c7cae6da39210e11
Miguel A. Angulo <maangulo12@gmail.com>
2016-01-03 22:35:37 -0600

    chore(views): update views to use blueprints

--- app/views.py
+++ app/views.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 """
     app.views
     ~~~~~~~~~~~~~~~~~~
@@ -8,17 +7,18 @@
     This module implements all of the views/routes of this application.
 """
 
-from flask import make_response, render_template
-
-from app import app
+from flask import Blueprint, make_response, render_template
 
 
-@app.route('/', defaults={'url': ''})
-@app.route('/<path:url>')
+bp = Blueprint('views', __name__)
+
+
+@bp.route('/', defaults={'url': ''})
+@bp.route('/<path:url>')
 def catch_all(url):
     return render_template('index.html')
 
 
-@app.errorhandler(404)
+@bp.errorhandler(404)
 def page_not_found(e):
     return make_response(open('app/templates/error/404_error.html').read())
