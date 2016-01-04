36e8964bf0ee760b569dd90a878e43e5aa635692
Miguel Angulo <maangulo12@gmail.com>
2016-01-04 13:07:36 -0600

    feat(admin): add admin modelview for models

--- app/admin.py
+++ app/admin.py
@@ -14,3 +14,10 @@ from app.models import User, Project, Category, Item, Expenditure, Fund, Draw, S
 
 
 admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Project, db.session))
+admin.add_view(ModelView(Category, db.session))
+admin.add_view(ModelView(Item, db.session))
+admin.add_view(ModelView(Expenditure, db.session))
+admin.add_view(ModelView(Fund, db.session))
+admin.add_view(ModelView(Draw, db.session))
+admin.add_view(ModelView(Subcontractor, db.session))
