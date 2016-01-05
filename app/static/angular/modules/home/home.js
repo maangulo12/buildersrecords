ccda6a356e87efb480748e246314a6452030853d
Miguel Angulo <maangulo12@gmail.com>
2016-01-05 15:52:00 -0600

    feat(home): add iife

--- app/static/angular/modules/home/home.js
+++ app/static/angular/modules/home/home.js
@@ -1,18 +1,24 @@
-var app = angular.module('app.home', []);
+(function() {
+    'use strict';
 
-app.config(function($stateProvider) {
-    $stateProvider.state('home', {
-        url: '/',
-        views: {
-            'nav': {
-                templateUrl: 'static/angular/components/navs/nav1.html'
-            },
-            'body': {
-                templateUrl: 'static/angular/modules/home/home.html',
-                controller: function(store) {
-                    store.remove('jwt');
-                }
+    angular
+        .module('app.home', [])
+        .config(
+            function($stateProvider) {
+                $stateProvider.state('home', {
+                    url: '/',
+                    views: {
+                        'nav': {
+                            templateUrl: 'static/angular/components/navs/nav1.html'
+                        },
+                        'body': {
+                            templateUrl: 'static/angular/modules/home/home.html',
+                            controller: function(store) {
+                                store.remove('jwt');
+                            }
+                        }
+                    }
+                });
             }
-        }
-    });
-});
+        );
+})();
