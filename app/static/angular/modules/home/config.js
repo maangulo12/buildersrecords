d183ba688b5611e973152479c308c690a1a57ea1
Miguel Angulo <maangulo12@gmail.com>
2016-01-05 20:13:08 -0600

    cleaup(home): cleanup function

--- app/static/angular/modules/home/config.js
+++ app/static/angular/modules/home/config.js
@@ -6,18 +6,17 @@
         .config(config);
 
     function config($stateProvider) {
-        $stateProvider
-            .state('home', {
-                url: '/',
-                views: {
-                    'nav': {
-                        templateUrl: 'static/angular/components/navs/nav1.html'
-                    },
-                    'body': {
-                        templateUrl: 'static/angular/modules/home/home.html',
-                        controller: 'HomeController'
-                    }
+        $stateProvider.state('home', {
+            url: '/',
+            views: {
+                'nav': {
+                    templateUrl: 'static/angular/components/navs/nav1.html'
+                },
+                'body': {
+                    templateUrl: 'static/angular/modules/home/home.html',
+                    controller: 'HomeController'
                 }
-            });
+            }
+        });
     }
 })();
